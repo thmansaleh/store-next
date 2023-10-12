68c8bf3f2d4a275140cf0c5e8cb82617dc0daeb4
thmansaleh <thman.saleh@gmail.com>
2023-10-13 01:18:07 +0400

    2 deploy

--- app/components/navigation/Icon.js
+++ app/components/navigation/Icon.js
@@ -2,19 +2,18 @@
 
 import { useDispatch,useSelector } from 'react-redux';
 import { setNav } from '@/app/store/features/navigationSlice';
+import {useEffect, useRef } from 'react';
 
 function Icon({svg,iconParent,dot,name}) {
     const dispatch = useDispatch();
-    // const svgId = document.getElementById(`${useSelector(state => state.navigation.name)}`);
-    // svgId?svgId.style.fill='black':null
-
-
-    const setPage =()=>{
-        dispatch(setNav(name))
-
-    }
+    const pageName = useSelector(state => state.navigation.name);
+    const icon = useRef(null)
+    useEffect(() => {  
+      dot?icon.current.childNodes[0].style.fill='black':icon.current.childNodes[0].style.fill='#A8A9B6'
+    }, [pageName]);
+    const setPage =()=>{ dispatch(setNav(name)) }
   return (
-    <div onClick={setPage}
+    <div ref={icon} onClick={setPage}
      style={iconParent?{marginBottom:'8px'}:{marginBottom:'0px'}}>
         {svg}
         <div style={iconParent?{bottom:'0'}:{bottom:'-50%'}} className="dot"></div>
