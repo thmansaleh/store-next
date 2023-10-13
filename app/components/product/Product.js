06a2537febfc685d21caae579afafa45a3d9c432
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-13 13:47:58 +0400

    Update Product.js

--- app/components/product/Product.js
+++ app/components/product/Product.js
@@ -1,1 +1,14 @@
+"use client"
 
+function Product() {
+  
+return  <div class="product">
+    <img srcset="" src="https://i.dummyjson.com/data/products/2/thumbnail.jpg" alt="">
+    <span class="product-name">iPhone 12</span>
+    <div
+    class="price-container">
+      <div class="previous-price">700 AED </   div>
+    </div>
+  </div>
+
+}
