317d46fa51d70260b18cb42fdd3e25f9328447f2
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-13 19:31:41 +0400

    Update Product.js

--- app/components/product/Product.js
+++ app/components/product/Product.js
@@ -5,7 +5,7 @@ export default function Product(){
     <img srcset="" src="https://i.dummyjson.com/data/products/2/thumbnail.jpg" alt="">
     <span className="product-name">iPhone 12</span>
     <div
-    class="price-container">
+    className="price-container">
       <div className="previous-price">700 AED </div>
     </div>
   </div>
