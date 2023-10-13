e6d044b4ef29d33696e8232c4d537924f239b9db
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-13 19:29:59 +0400

    Update Product.js

--- app/components/product/Product.js
+++ app/components/product/Product.js
@@ -1,12 +1,12 @@
 "use client"
 import "./style.css"
 export default function Product(){
-  return  <div class="product">
+  return  <div className="product">
     <img srcset="" src="https://i.dummyjson.com/data/products/2/thumbnail.jpg" alt="">
-    <span class="product-name">iPhone 12</span>
+    <span className="product-name">iPhone 12</span>
     <div
     class="price-container">
-      <div class="previous-price">700 AED </div>
+      <div className="previous-price">700 AED </div>
     </div>
   </div>
 }
