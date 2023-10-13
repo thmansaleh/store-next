142199101566a5cab661a6d2d66ce65fe01ba948
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-13 14:38:23 +0400

    Update Products.js

--- app/components/product/Products.js
+++ app/components/product/Products.js
@@ -1,6 +1,7 @@
 "use client"
-import './style.css' 
-import Product from "./Product"
+import './style.css' ;
+
+import Product from "./Product";
   
  export default function Products() {
    return <div className="products-patient"> 
