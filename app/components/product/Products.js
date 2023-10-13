3515bfde16345aa3d57b09d3faa29b20671502d0
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-13 14:11:52 +0400

    Update Products.js

--- app/components/product/Products.js
+++ app/components/product/Products.js
@@ -1,10 +1,10 @@
-import Product from './Product.js"
+import Product from './Product"
  import './style.css' 
   
  export default Products Home() { 
 const data=[1,1,1,1,1,1,1]
    return <div className="products-patient"> 
-   {data.map((e,i)/>{
+   {data.map((e,i)=>{
    return <product/>
 })
 
