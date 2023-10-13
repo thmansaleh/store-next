e116b98d73a8469c6feb3d967a48292160accb0c
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-13 14:30:12 +0400

    Update Products.js

--- app/components/product/Products.js
+++ app/components/product/Products.js
@@ -3,12 +3,7 @@ import Product from './Product"
  import './style.css' 
   
  export default function Products() {
-const data=[1,1,1,1,1,1,1]
    return <div className="products-patient"> 
-  
-   <Product/>
-
-
-   
-   </div> 
+  <Product/>
+</div> 
  }
