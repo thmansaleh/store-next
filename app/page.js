2238025e39c9b9e94230a36e6c204d74bc012bf3
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-13 19:15:46 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 
 import Navigation from './components/navigation/Navigation'
-import Products from './components/product/Products'
 import styles from './page.module.css'
 
 export default function Home() {
   return <>
   <Navigation/>
-  <Products/>
   </>
 }
