52d26055ba783de3f6be289710e3800bc92a578b
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-14 01:08:48 +0400

    Update Slider.js

--- app/product/Slider.js
+++ app/product/Slider.js
@@ -25,6 +25,14 @@ navigation Scrollbar pagination={{ clickable: true }}
   
     <img className="w-100 product-img-slider" src='https://media.cnn.com/api/v1/images/stellar/prod/170321162454-dubai-police-bugatti.jpg?q=h_619,w_1024,x_0,y_31,c_crop/h_619,w_1100,c_lpad,b_rgb:061015/h_720,w_1280' alt="My Image" />
   </SwiperSlide>
+ <SwiperSlide style={{ textAlign: 'center', height: '50vh', width: '100%', margin: 0 }}>
+  
+    <img className="w-100 product-img-slider" src='https://media.cnn.com/api/v1/images/stellar/prod/170321162454-dubai-police-bugatti.jpg?q=h_619,w_1024,x_0,y_31,c_crop/h_619,w_1100,c_lpad,b_rgb:061015/h_720,w_1280' alt="My Image" />
+  </SwiperSlide>
+ <SwiperSlide style={{ textAlign: 'center', height: '50vh', width: '100%', margin: 0 }}>
+  
+    <img className="w-100 product-img-slider" src='https://media.cnn.com/api/v1/images/stellar/prod/170321162454-dubai-police-bugatti.jpg?q=h_619,w_1024,x_0,y_31,c_crop/h_619,w_1100,c_lpad,b_rgb:061015/h_720,w_1280' alt="My Image" />
+  </SwiperSlide>
 
 </Swiper>
 
