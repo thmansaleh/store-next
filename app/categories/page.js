b252ca4dd239be665d103855ae669950dd05cff0
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-17 17:34:23 +0400

    Update page.js

--- app/categories/page.js
+++ app/categories/page.js
@@ -4,9 +4,8 @@ import './style.css'
 import Category from './Category'
 import SetNave from '../components/SetNave'
 export default function Categories(){
-
+ SetNave({name:"categories",show:true)
   return <div className="categories" >
-        <SetNave name='categories' show={true}/>
       <Category 
       name="عبايات ملونة"
       img="https://kitchen.sayidaty.net/uploads/small/f5/f5275c258d4c03b1fc8b1ab888ec642a_w240_h240.jpg"
