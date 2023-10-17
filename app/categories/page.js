161000a461f5dc962f74626faf58f9f38443aa45
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-17 16:58:17 +0400

    Update page.js

--- app/categories/page.js
+++ app/categories/page.js
@@ -1,1 +1,13 @@
 
+
+import './style.css'
+import Category from './Category'
+
+export default function Categories(){
+
+  return <Category 
+    name="عبايات ملونة"
+      img="https://kitchen.sayidaty.net/uploads/small/f5/f5275c258d4c03b1fc8b1ab888ec642a_w240_h240.jpg"
+    
+    />
+}
