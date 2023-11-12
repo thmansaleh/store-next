357ec27071e685c927c39a0981eb6f01a389f6f0
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-11-13 02:35:43 +0400

    Update Description.js

--- app/product/description/Description.js
+++ app/product/description/Description.js
@@ -1,44 +1,21 @@
-import './style.css'
+
 
 const Description = () => {
 
-return <div className="product-description-container">
-      <div className="icons-container">
-        <div>
-
-          <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M18.581,2.14,12.316.051a1,1,0,0,0-.632,0L5.419,2.14A4.993,4.993,0,0,0,2,6.883V12c0,7.563,9.2,11.74,9.594,11.914a1,1,0,0,0,.812,0C12.8,23.74,22,19.563,22,12V6.883A4.993,4.993,0,0,0,18.581,2.14ZM20,12c0,5.455-6.319,9.033-8,9.889-1.683-.853-8-4.42-8-9.889V6.883A3,3,0,0,1,6.052,4.037L12,2.054l5.948,1.983A3,3,0,0,1,20,6.883Z" /><path d="M15.3,8.3,11.112,12.5,8.868,10.16a1,1,0,1,0-1.441,1.386l2.306,2.4a1.872,1.872,0,0,0,1.345.6h.033a1.873,1.873,0,0,0,1.335-.553l4.272-4.272A1,1,0,0,0,15.3,8.3Z" /></svg>
-
-          <span>1 Year Guarantee</span>
-
-        </div>
-        <div>
-
-          <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512"><path d="M12,16c-2.206,0-4-1.794-4-4s1.794-4,4-4,4,1.794,4,4-1.794,4-4,4Zm0-6c-1.103,0-2,.897-2,2s.897,2,2,2,2-.897,2-2-.897-2-2-2Zm-7-3c-.552,0-1,.448-1,1s.448,1,1,1,1-.448,1-1-.448-1-1-1Zm13,3c0,.552,.448,1,1,1s1-.448,1-1-.448-1-1-1-1,.448-1,1Zm-13,3c-.552,0-1,.448-1,1s.448,1,1,1,1-.448,1-1-.448-1-1-1Zm13,3c0,.552,.448,1,1,1s1-.448,1-1-.448-1-1-1-1,.448-1,1Zm-.999,5c-1.64,0-3.488-.528-5.275-1.039-1.654-.473-3.365-.961-4.725-.961-2.731,0-5.549,1.303-5.578,1.315l-1.423,.666V7.158c0-1.128,.618-2.148,1.615-2.663,1.325-.683,3.333-1.496,5.386-1.496,1.662,0,3.266,.535,4.816,1.051,1.463,.488,2.845,.949,4.183,.949,1.386,0,2.517-.362,3.426-.653,.302-.097,.577-.185,.828-.251,.898-.239,1.835-.048,2.573,.52,.746,.574,1.174,1.443,1.174,2.384v12.285l-.587,.266c-.131,.06-3.233,1.449-6.413,1.449Zm-10-4c1.64,0,3.488,.528,5.275,1.039,1.655,.473,3.365,.961,4.725,.961,2,0,4.03-.665,5-1.032V7c0-.316-.144-.607-.394-.799-.142-.109-.442-.276-.842-.171-.221,.059-.464,.137-.73,.222-.986,.315-2.338,.748-4.035,.748-1.662,0-3.266-.535-4.816-1.051-1.463-.488-2.845-.949-4.183-.949-1.739,0-3.547,.798-4.47,1.273-.328,.169-.532,.508-.532,.885v10.782c1.23-.427,3.103-.94,5.002-.94Z" /></svg>
-
-          <span>Delivery Fee 20 AED</span>
-        </div>
-      </div>
-
-
-
-      <div className="product-description">
-        <span>Description</span>
-        <p>hgjifkopd[</p>
-
-      </div>
-      <div className="product-highlights">
-        <span>Highlights</span>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-    
-
-        </ul>
-      </div>
-
-
-    </div>
+return <div className="max-w-sm mx-auto  bg-white ">
+  <h2 className="text-md font-bold mb-4">Description</h2>
+  <p className="text-gray-700 mb-4 text-sm ">Your out-and-about look just took a classy turn with our roomy little Grydy crossbody. Two compartment storage and front zip pocket keeps all the essentials just where you need them.</p>
+  <h3 className="text-md font-semibold mb-2">Highlights</h3>
+  <ul className="list-disc pl-5 mb-4 text-sm text-gray-600">
+    <li>Name: GRYDY Quilted Crossbody Bag</li>
+    <li>Color: Yellow</li>
+    <li>Brand Name: Aldo</li>
+    {/* Add more list items here */}
+  </ul>
+  <div className="pt-4 border-t">
+    <p className="text-sm text-gray-500">SKU: 23GRYDY-740-YELLOW</p>
+  </div>
+</div>
   
 }
 
