4a67cdd73dd75a714542b4d48606d9423b38c4ef
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-11-13 01:27:55 +0400

    Update page.js

--- app/settings/page.js
+++ app/settings/page.js
@@ -1,1 +1,15 @@
-bb
+
+
+export default function(){
+
+return <div dir="ltr" className="bg-white shadow rounded">
+  <div className="flex items-center justify-between p-4 border-b relative ">
+    <span className="text-gray-800 text-sm font-semibold">Location</span>
+    <span className="text-gray-500 px-6 text-sm">UAE</span>
+  </div>
+  <div className="flex items-center justify-between p-4 border-b ">
+    <span className="text-gray-800 text-sm font-semibold">Language</span>
+    <span className="text-gray-500 px-6 text-sm">Arabic</span>
+  </div>
+</div>
+}
