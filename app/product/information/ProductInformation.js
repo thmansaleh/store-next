fa45cb054f20418342ff99529f8cb685779cd599
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-11-13 02:17:17 +0400

    Update ProductInformation.js

--- app/product/information/ProductInformation.js
+++ app/product/information/ProductInformation.js
@@ -1,10 +1,16 @@
-"use client"
-import './style.css'
+
 export default function ProductInformation(){
-  return <div className="product-information">
-        <span>iPhone 12</span>
-        <span className="product-description">ROUND TOE LOW TOP SNEAKERS</span>
-        <div className="product-prices-container"><div><span>899 AED</span><span>1060 AED</span></div><span>On Sale -17.94% Off</span></div>
-        <div className="product-delivery-title"><span>Delivery by </span><span>10 May, Wednesday</span></div>
-      </div>
+  return <div className="bg-white p-4 shadow rounded-lg">
+  <div className="text-lg font-bold text-gray-800">NINE WEST</div>
+  <div className="text-sm text-gray-500">FANTASY EMBELLISHED POINTED TOE PUMPS</div>
+  <div className="flex items-baseline mt-2">
+    <div className="text-xl font-bold text-red-600">AED 149</div>
+    <div className="text-sm text-gray-500 line-through ml-2">AED 449</div>
+    <div className="text-sm font-semibold text-red-500 ml-2">On Sale -67% Off</div>
+  </div>
+  <div className="text-sm text-gray-500 mt-1">Delivery by <span className="text-green-400">16 Nov, Thursday</span></div>
+  <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 focus:outline-none focus:shadow-outline">
+    Select Area
+  </button>
+</div>
 }
