6dba17382c5c9ee12d0c312bd98559983bca9c67
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-11-13 20:38:15 +0400

    Update cart

---
+++ app/cart/OrderDetailes.js
@@ -0,0 +1,17 @@
+export default function (){
+return <div className="px-4 py-2 font-sans bg-gray-50">
+  <div className="text-md font-semibold">ORDER DETAILS</div>
+  <div className="flex justify-between  py-2">
+    <span>Subtotal</span>
+    <span className="font-semibold">AED 1415</span>
+  </div>
+  <div className="flex justify-between border-b py-2">
+    <span>Shipping fee</span>
+    <span className="ml-2 text-green-600 font-semibold">FREE</span>
+  </div>
+  <div className="flex justify-between pt-2">
+    <span className="font-semibold">Total Amount</span>
+    <span className="font-semibold">AED 1415</span>
+  </div>
+</div>
+}
