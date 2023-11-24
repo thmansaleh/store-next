98810f51b730fcf7a7d6da146bcba237df244640
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-11-24 22:15:23 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import styles from './page.module.css'
 import Product from './components/product/Product'
 import SetNave from './components/SetNave'
 import Footer from './components/footer/Footer'
+import SearchInput from "./components/SearchInput"
 
 export default function Home() {
 
   return <>
 
   <SetNave name='/' show={true}/>
+<SearchInput/>
   <div style={{display:'flex',justifyContent:'space-around',flexWrap:'wrap'}}>
 
   <Product/>
