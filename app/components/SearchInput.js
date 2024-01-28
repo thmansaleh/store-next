1bf88c0dfc3e8a7450dbcddf9a2bef345c70446d
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-01-28 14:16:36 +0400

    Update SearchInput.js

--- app/components/SearchInput.js
+++ app/components/SearchInput.js
@@ -2,13 +2,12 @@
 export default function SearchInput(){
 
 
-return <div className=" relative   w-full ">
-  <svg className="absolute fill-gray-400 mx-2 right-0 translate-y-1/2" stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 512 512" height={25} width={25} xmlns="http://www.w3.org/2000/svg"><path d="M289.94 256l95-95A24 24 0 00351 127l-95 95-95-95a24 24 0 00-34 34l95 95-95 95a24 24 0 1034 34l95-95 95 95a24 24 0 0034-34z" /></svg>
-  <input placeholder="what are you looking for?" className="w-full p-3 rounded-lg focus:border-gray-400 border-2 outline-0 border-inherit text-sm" />
-  <div className="absolute rounded-lg text-slate-700 bg-gray-50 text-sm p-2 w-full mt-1 ">
-    <div className=" p-3 border-b">xjdid</div>
-    <div className=" p-3 border-b">xjdid</div>
-    <div className=" p-3">xjdid</div>
+return <div className="  mb-2 flex items-center justify-between">
+  <svg className="w-20 object-contain h-10 p-2  fill-back-500 bg-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={512} height={512}><g id="_01_align_center" data-name="01 align center"><path d="M23.259,16.2l-2.6-9.371A9.321,9.321,0,0,0,2.576,7.3L.565,16.35A3,3,0,0,0,3.493,20H7.1a5,5,0,0,0,9.8,0h3.47a3,3,0,0,0,2.89-3.8ZM12,22a3,3,0,0,1-2.816-2h5.632A3,3,0,0,1,12,22Zm9.165-4.395a.993.993,0,0,1-.8.395H3.493a1,1,0,0,1-.976-1.217l2.011-9.05a7.321,7.321,0,0,1,14.2-.372l2.6,9.371A.993.993,0,0,1,21.165,17.605Z" /></g></svg>
+  <img className=" rounded-lg w-1/3  obgject-contain" src="https://1000logos.net/wp-content/uploads/2021/05/Shein-logo-768x432.png" alt="j" />
+  <div className="flex px-2  ">
+    <svg className="w-10 h-10 p-2  fill-back-500 bg-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={512} height={512}><g id="_01_align_center" data-name="01 align center"><path d="M24,22.586l-6.262-6.262a10.016,10.016,0,1,0-1.414,1.414L22.586,24ZM10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18Z" /></g></svg>
+    <svg className="w-10 h-10 p-2  fill-back-500 bg-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={512} height={512}><g id="_01_align_center" data-name="01 align center"><path d="M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917ZM12,20.846c-3.253-2.43-10-8.4-10-12.879a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,7.967h2a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,7.967C22,12.448,15.253,18.416,12,20.846Z" /></g></svg>
   </div>
 </div>
 }
