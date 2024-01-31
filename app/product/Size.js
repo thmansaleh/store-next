dbd17d7a9c2f8234f9de562edf5d1231edbe7e89
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-01-31 14:33:45 +0400

    Update Size.js

--- app/product/Size.js
+++ app/product/Size.js
@@ -1,4 +1,17 @@
 export default function Size(){
-  return 
+  return <div className="p-2 bg-grbay-200">
+  <div className="flex py-1 
+  text-sm justify-between items-center bg-white">
+    <div>Size</div>
+    <div className="p-2 bg-zinc-100 rounded-full text-gray-600">Size guide</div>
+  </div>
+  <div className="text-sm my-2 flex w-full overflow-auto bg-white py-1  ">
+    <div className="text-sm  text-gray-600 mx-2 px-6 py-2 bg-zinc-100 rounded-full hover:border-black hover:border hover:text-black">S</div>
+    <div className="hover:text-black hover:border-black hover:border  text-sm mx-2 px-6 py-2 bg-zinc-100 rounded-full text-gray-600">M</div>
+    <div className=" hover:text-black hover:border-black hover:border text-sm  mx-2 px-6 py-2 bg-zinc-100 rounded-full text-gray-600">L</div>
+    <div className=" hover:text-black text-sm hover:border-black hover:border  mx-2 px-6 py-2 bg-zinc-100 rounded-full text-gray-600">XL</div>
+    <div className="hover:text-black hover:border-black hover:border text-sm mx-2 px-6 py-2 bg-zinc-100 rounded-full text-gray-600">XXL</div>
+  </div>
+</div>
 
 }
