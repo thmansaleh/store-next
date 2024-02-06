c21d7601f562615df12d2270fc96fe2fbf18e2ad
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-07 00:47:57 +0400

    Update Category.js

--- app/categories/Category.js
+++ app/categories/Category.js
@@ -1,9 +1,30 @@
-"use client" 
-  import Link from "next/link"
- export default function Category({name,img}){ 
+
+ export default function Categories(){ 
   
-   return <Link href='/'> 
-       <img  src={img} alt="" /> 
-        <span>{name}</span> 
-     </Link> 
+   return <div className="w-36 select-none bg-gray-200  overflow-auto">
+  <div className="w-full py-3  text-sm text-black px-2 font-semibold  bg-black  text-white "> عبايات</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> ملابس داخلية</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">رجالي</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">ملابس أطفال</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">إلكترونيات</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> سماعات</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> أحذية</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">هواتف ذكية</div>
+  <div className="w-full py-3  text-sm text-black px-2 font-semibold   "> عبايات</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold hover:border-b-2 border-gray-900"> ملابس داخلية</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">رجالي</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">ملابس أطفال</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">إلكترونيات</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> سماعات</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> أحذية</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">هواتف ذكية</div>
+  <div className="w-full py-3  text-sm text-black px-2 font-semibold   "> عبايات</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold hover:border-b-2 border-gray-900"> ملابس داخلية</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">رجالي</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">ملابس أطفال</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">إلكترونيات</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> سماعات</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> أحذية</div>
+  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">هواتف ذكية</div>
+</div>
  }
