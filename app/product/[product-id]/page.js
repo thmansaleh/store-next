0fafccbfc06f677ffaf1d8eb34c2f8ed2ba8cc3d
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-06 18:44:25 +0400

    Update page.js

--- app/product/[product-id]/page.js
+++ app/product/[product-id]/page.js
@@ -2,12 +2,14 @@ import Flip from '@/app/components/flip/Flip'
 import Slider from '../Slider'
 import ProductInformation from '@/app/product/information/ProductInformation'
 import Description from '../description/Description'
+import FreeDelivery from '@/app/product/FreeDelivery'
 import Size from '@/app/product/Size'
 export default function ProductId() {
   return <> 
     <Slider/>
     <ProductInformation/>
     <Size/>
+<FreeDelivery/>
     <Flip/>
     <Description/>
 
