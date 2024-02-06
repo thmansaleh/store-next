f11be96031e031bd29e50f87b08ad1058d1a9155
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-07 00:59:39 +0400

    Update categories

---
+++ app/categories/Brands.js
@@ -0,0 +1,19 @@
+export default function Brands(){
+ return <div className=" my-2 w-full select-none bg-white">
+  <span className="text-sm font-semibold p-2 block">البراندات</span>
+  <div className="w-full  select-none   h-fit flex justify-around flex-wrap p-1 ">
+    <div className="px-2   my-2    text-center">
+      <img className="w-16 h-16 inline-block object-contain" src="https://alkalej.com/cdn/shop/files/PhotoRoom_047_20230813_024400.jpg?v=1693780269&width=1000" alt="qqq" />
+      <span className="text-xs py-1 block font-semibold ">عبايات ملونة</span>
+    </div>
+    <div className="px-2 my-2   text-center">
+      <img className="w-16 h-16 inline-block object-contain" src="https://alkalej.com/cdn/shop/files/11_2d484484-9265-462d-8bf7-039a27fdd236.jpg?v=1704819990&width=1000" alt="qqq" />
+      <span className="text-xs py-1 block font-semibold ">عبايات سوداء</span>
+    </div>
+    <div className="px-2 my-2   text-center">
+      <img className="w-16 h-16 inline-block object-contain" src="https://img.kwcdn.com/product/open/2023-11-17/1700200320924-968d7c34c175480ca6ac8aa10a6eadec-goods.jpeg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp" alt="qqq" />
+      <span className="text-xs py-1 block font-semibold ">عبايات شتوية</span>
+    </div>
+  </div>
+</div>
+}
