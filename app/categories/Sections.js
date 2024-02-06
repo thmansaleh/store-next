29c750748f2b509e931fb120d4e5486dd15a4de3
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-07 00:53:27 +0400

    Update categories

---
+++ app/categories/Sections.js
@@ -0,0 +1,21 @@
+export default function Sections(){
+  return <div className="w-full  select-none   h-fit flex justify-around flex-wrap p-1">
+  <div className="px-2   my-2  bg-white rounded-lg text-center">
+    <img className="w-16 h-16 inline-block object-contain" src="https://alkalej.com/cdn/shop/files/PhotoRoom_047_20230813_024400.jpg?v=1693780269&width=1000" alt="qqq" />
+    <span className="text-xs py-1 text-gray-700 block font-semibold ">عبايات ملونة</span>
+  </div>
+  <div className="px-2 my-2  bg-white rounded-lg text-center">
+    <img className="w-16 h-16 inline-block object-contain" src="https://alkalej.com/cdn/shop/files/11_2d484484-9265-462d-8bf7-039a27fdd236.jpg?v=1704819990&width=1000" alt="qqq" />
+    <span className="text-gray-700 text-xs py-1 block font-semibold ">عبايات سوداء</span>
+  </div>
+  <div className="px-2 my-2   bg-white rounded-lg text-center">
+    <img className="w-16 h-16 inline-block object-contain" src="https://img.kwcdn.com/product/open/2023-11-17/1700200320924-968d7c34c175480ca6ac8aa10a6eadec-goods.jpeg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp" alt="qqq" />
+    <span className="text-gray-700 text-xs py-1 block font-semibold ">عبايات شتوية</span>
+  </div>
+  <div className="px-2 min-w-24 my-2 py-1  bg-white rounded-lg text-center max-w-24">
+    <img className="w-16 h-16 inline-block object-contain" src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/4e4646ce546c621c38d893c8540db2f3.jpg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp" alt="qqq" />
+    <span className="text-gray-700 text-xs py-1 block font-semibold ">مخاوير </span>
+  </div>
+</div>
+
+}
