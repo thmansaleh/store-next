ac759ae80043cf21877fd28a13135a676141b8bb
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-07 00:50:53 +0400

    Update page.js

--- app/categories/page.js
+++ app/categories/page.js
@@ -1,20 +1,19 @@
 
-
-import './style.css'
-import Category from './Category'
+import Categories from './Categories'
 import SetNave from '../components/SetNave'
 
 export default function () {
 
-  return <div className="categories" >
+  return <>
           <SetNave name='categories' show={true}/>
 
-    {[1, 1, 1,1,1, 1].map(e => {
-      return <Category
-        name="عبايات ملونة"
-        img="https://kitchen.sayidaty.net/uploads/small/f5/f5275c258d4c03b1fc8b1ab888ec642a_w240_h240.jpg"
+    <div dir="rtl" className="bg-gray-100 w-full  flex h-dvh">
+ 
 
-      />
-    })}
-  </div>
+<Categories/>
+
+
+
+</div>
+  </>
 }
