dbbfc9d6c51736b8e4497eec65fabf28a3456bea
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-07 15:37:37 +0400

    Update page.js

--- app/product/[product-id]/page.js
+++ app/product/[product-id]/page.js
@@ -1,6 +1,7 @@
 import Flip from '@/app/components/flip/Flip'
 import Slider from '../Slider'
 import ProductInformation from '@/app/product/information/ProductInformation'
+import AddToCartButton from "@app/product/AddToCartButton"
 import Description from '../description/Description'
 import FreeDelivery from '@/app/product/FreeDelivery'
 import Size from '@/app/product/Size'
@@ -14,6 +15,7 @@ export default function ProductId() {
 <FreeDelivery/>
     <Flip/>
     <Description/>
+<AddToCartButton/>
 </div>
    
     </>
