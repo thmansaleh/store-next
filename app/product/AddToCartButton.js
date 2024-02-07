80a1b9a13bdff3e039b27353363feb2412d24fa3
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-07 16:30:33 +0400

    Update AddToCartButton.js

--- app/product/AddToCartButton.js
+++ app/product/AddToCartButton.js
@@ -1,6 +1,6 @@
 export default function AddToCartButton(){
 
-return <div className="w-full bg-white select-none px-1 py-3 sticky bottom-0">
+return <div className="w-full bg-white select-none p-3 sticky bottom-0">
   <button className="rounded-lg w-full bg-black text-sm font-semibold text-white py-2">Add To Cart</button>
 </div>
 
