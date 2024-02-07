b300d52fea3dc534450dcdd95f02757ca6097d32
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-07 08:00:28 +0400

    Update ProductInformation.js

--- app/product/information/ProductInformation.js
+++ app/product/information/ProductInformation.js
@@ -1,6 +1,6 @@
 
 export default function ProductInformation(){
-  return <div className="bg-white py-4 shadow rounded-lg">
+  return <div className="bg-white py-4 ">
   <div className="text-lg font-bold text-gray-800">NINE WEST</div>
   <div className="text-sm text-gray-500">FANTASY EMBELLISHED POINTED TOE PUMPS</div>
   <div className="flex items-baseline mt-2">
@@ -9,8 +9,6 @@ export default function ProductInformation(){
     <div className="text-sm font-semibold text-red-500 ml-2">On Sale -67% Off</div>
   </div>
   <div className="text-sm text-gray-500 mt-1">Delivery by <span className="text-green-400">16 Nov, Thursday</span></div>
-  <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 focus:outline-none focus:shadow-outline">
-    Select Area
-  </button>
+
 </div>
 }
