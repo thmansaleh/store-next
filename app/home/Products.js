75e9573f7e4c37309318fad846d312ac03f33a37
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-14 15:27:18 +0400

    Update home

---
+++ app/home/Products.js
@@ -0,0 +1,49 @@
+export default function Products(){
+
+return <div className="p-2 flex wrap justify-around bg-white  flex-wrap">
+  <a href="hthhh" className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
+    <span className=" bg-red-700 text-white top-0 right-0 text-center absolute rounded-bl-lg px-3 text-xs bg-blend-multiply line-height opacity-80 ">خصم 20%</span>
+    <div className="  ">
+      <img className="h-48 w-full object-contain " src="https://jomla.ae/_next/image/?url=https%3A%2F%2Fwp.jomla.ae%2Fwp-content%2Fuploads%2F2022%2F11%2F71LinfGHuL._AC_SX466_.jpg&w=750&q=75" alt />
+    </div>
+    <span className=" line-clamp-2 text-sm my-2 px-2">باور بانك أنكر 24000 مللي أمبير Anker 737 Power Bank (PowerCore 24K)</span>
+    <div className="p-2 text-sm bg-zinc-100  ">
+      <span>190</span>
+      <span className="text-green-600">د.أ</span>
+    </div>
+  </a>
+  <a href="hthhh" className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
+    <span className=" bg-red-700 text-white top-0 right-0 text-center absolute rounded-bl-lg px-3 text-xs bg-blend-multiply line-height opacity-80 ">خصم 20%</span>
+    <div className="  ">
+      <img className="h-48 w-full object-contain " src="https://jomla.ae/_next/image/?url=https%3A%2F%2Fwp.jomla.ae%2Fwp-content%2Fuploads%2F2023%2F12%2Fnubia-red-magic-9-pro-12gb256gb-silver.jpg&w=750&q=75" alt />
+    </div>
+    <span className=" line-clamp-2 text-sm my-2 px-2">باور بانك أنكر 24000 مللي أمبير Anker 737 Power Bank (PowerCore 24K)</span>
+    <div className="p-2 text-sm bg-zinc-100  ">
+      <span>190</span>
+      <span className="text-green-600">د.أ</span>
+    </div>
+  </a>
+  <a href="hthhh" className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
+    <span className=" bg-red-700 text-white top-0 right-0 text-center absolute rounded-bl-lg px-3 text-xs bg-blend-multiply line-height opacity-80 ">خصم 20%</span>
+    <div className="  ">
+      <img className=" h-48 w-full object-contain " src="https://jomla.ae/_next/image/?url=https%3A%2F%2Fwp.jomla.ae%2Fwp-content%2Fuploads%2F2023%2F05%2F%25D8%25B9%25D8%25B1%25D8%25B6-%25D8%25B4%25D8%25A7%25D8%25AD%25D9%2586-%25D9%2582%25D9%2588%25D9%258A-3-%25D8%25B4%25D8%25A7%25D8%25AD%25D9%2586.jpg&w=750&q=75" alt />
+    </div>
+    <span className=" line-clamp-2 text-sm my-2 px-2">باور بانك أنكر 24000 مللي أمبير Anker 737 Power Bank (PowerCore 24K)</span>
+    <div className="p-2 text-sm bg-zinc-100  ">
+      <span>190</span>
+      <span className="text-green-600">د.أ</span>
+    </div>
+  </a>
+  <a href="hthhh" className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   hover:shadow-md ">
+    <span className=" bg-red-700 text-white top-0 right-0 text-center absolute rounded-bl-lg px-3 text-xs bg-blend-multiply line-height opacity-80 ">خصم 20%</span>
+    <div className="  ">
+      <img className=" h-48 w-full object-contain " src="https://linkconnects.com/uploads/user_images/cfa0860e83a4c3a763a7e62d825349f7.jpg" alt />
+    </div>
+    <span className=" line-clamp-2 text-sm my-2 px-2">باور بانك أنكر 24000 مللي أمبير Anker 737 Power Bank (PowerCore 24K)</span>
+    <div className="p-2 text-sm font-semibold bg-zinc-100  ">
+      <span>190</span>
+      <span className="text-green-600">د.أ</span>
+    </div>
+  </a>
+</div>
+}
