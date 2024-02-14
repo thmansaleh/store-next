b17f6a006ef6ae177a4df9f3d34127c44f102f7f
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-14 15:32:27 +0400

    Update Home.js

--- app/home/Home.js
+++ app/home/Home.js
@@ -1,4 +1,13 @@
+
+import SetNave from '@/app//components/SetNave'
+import SearchInput from "@/app/components/SearchInput"
+import Products from'@/app/home/Products'
 export default function Home(){
-  return <h1>home</h1>
+  return <>
+    <SetNave name='/' show={true}/>
+<SearchInput/>
+<Products/<
+
+</>
 
 }
