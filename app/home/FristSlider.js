8399605c49b7e2cf27f117dcf94d3198ed232d1a
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-19 02:03:38 +0400

    Update FristSlider.js

--- app/home/FristSlider.js
+++ app/home/FristSlider.js
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/pagination';
+//import 'swiper/css/pagination';
 
 
 //import { Pagination } from 'swiper/modules';
