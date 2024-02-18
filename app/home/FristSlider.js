dbb32f13a0caecca94478a0dc7aa23040a4a2c4b
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-18 23:03:34 +0400

    Update FristSlider.js

--- app/home/FristSlider.js
+++ app/home/FristSlider.js
@@ -1,10 +1,36 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+"use client"
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import './styles.css';
+
+//import { Pagination } from 'swiper/modules';
+
+export default function FristSlider() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={2.5}
+        spaceBetween={15}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper my-3 "
+      >
+        <SwiperSlide>
+  <img class="rounded-lg  h-64 object-contain border border-zinc-300 " src="https://m.media-amazon.com/images/I/31RJGO+PNNL._AC_SY580_.jpg" alt="">
+
+</SwiperSlide>
+
+<SwiperSlide>
+  <img class="rounded-lg  h-64 object-contain border border-zinc-300 " src="https://m.media-amazon.com/images/I/31RJGO+PNNL._AC_SY580_.jpg" alt="">
+
+        
+
+</>
+)
 
