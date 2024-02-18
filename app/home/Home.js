9484d54846b6d946c74f47adfc8f92121bcd47f3
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-18 23:08:58 +0400

    Update Home.js

--- app/home/Home.js
+++ app/home/Home.js
@@ -1,11 +1,13 @@
 
 import SetNave from '@/app//components/SetNave'
 import SearchInput from "@/app/components/SearchInput"
+import FristSlider from './FristSlider'
 import Products from'@/app/home/Products'
 export default function Home(){
   return <>
     <SetNave name='/' show={true}/>
 <SearchInput/>
+<FristSlider/>
 <Products/>
 
 </>
