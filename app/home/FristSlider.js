0aa92609cbcc37c1194700c36a3f45dbf5bbe244
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-18 22:58:26 +0400

    Update home

---
+++ app/home/FristSlider.js
@@ -0,0 +1,10 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
+
