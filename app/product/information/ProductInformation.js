329bfd371adb857ba39a57490e5d0b03100d3ffc
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-19 03:20:37 +0400

    Update ProductInformation.js

--- app/product/information/ProductInformation.js
+++ app/product/information/ProductInformation.js
@@ -4,11 +4,11 @@ export default function ProductInformation(){
   <div className="text-lg font-bold text-gray-800">NINE WEST</div>
   <div className="text-sm text-gray-500">FANTASY EMBELLISHED POINTED TOE PUMPS</div>
   <div className="flex items-baseline mt-2">
-    <div className="text-xl font-bold text-red-600">AED 149</div>
-    <div className="text-sm text-gray-500 line-through ml-2">AED 449</div>
-    <div className="text-sm font-semibold text-red-500 ml-2">On Sale -67% Off</div>
+    <div className="text-xl font-bold text-red-600">149 د.إ</div>
+    <div className="text-sm text-gray-500 line-through ml-2">499 د.إ</div>
+    <div className="text-sm font-semibold text-red-500 ml-2">تخفيض -60% عند الشراء </div>
   </div>
-  <div className="text-sm text-gray-500 mt-1">Delivery by <span className="text-green-400">16 Nov, Thursday</span></div>
+  <div className="text-sm text-gray-500 mt-1">توصيل في  <span className="text-green-400">16 Nov, Thursday</span></div>
 
 </div>
 }
