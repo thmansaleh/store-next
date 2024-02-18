9938d7d5378903462a1ea89e19514b178037acb8
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-18 23:10:05 +0400

    Update FristSlider.js

--- app/home/FristSlider.js
+++ app/home/FristSlider.js
@@ -26,10 +26,19 @@ export default function FristSlider() {
 
 </SwiperSlide>
 
-<SwiperSlide>
+   <SwiperSlide>
   <img class="rounded-lg  h-64 object-contain border border-zinc-300 " src="https://m.media-amazon.com/images/I/31RJGO+PNNL._AC_SY580_.jpg" alt="">
 
-        
+</SwiperSlide>
+   <SwiperSlide>
+  <img class="rounded-lg  h-64 object-contain border border-zinc-300 " src="https://m.media-amazon.com/images/I/31RJGO+PNNL._AC_SY580_.jpg" alt="">
+
+</SwiperSlide>
+   <SwiperSlide>
+  <img class="rounded-lg  h-64 object-contain border border-zinc-300 " src="https://m.media-amazon.com/images/I/31RJGO+PNNL._AC_SY580_.jpg" alt="">
+
+</SwiperSlide>
+      
 
 </>
 )
