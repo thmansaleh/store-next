3226a5863157fdc2b9665b5a63dee4bd171448a3
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-19 03:18:16 +0400

    Update Description.js

--- app/product/description/Description.js
+++ app/product/description/Description.js
@@ -3,13 +3,13 @@
 const Description = () => {
 
 return <div className="max-w-sm mx-auto  bg-white ">
-  <h2 className="text-md font-bold mb-4">Description</h2>
+  <h2 className="text-md font-bold mb-4">الوصف</h2>
   <p className="text-gray-700 mb-4 text-sm ">Your out-and-about look just took a classy turn with our roomy little Grydy crossbody. Two compartment storage and front zip pocket keeps all the essentials just where you need them.</p>
-  <h3 className="text-md font-semibold mb-2">Highlights</h3>
+  <h3 className="text-md font-semibold mb-2">المميزات</h3>
   <ul className="list-disc pl-5 mb-4 text-sm text-gray-600">
-    <li>Name: GRYDY Quilted Crossbody Bag</li>
-    <li>Color: Yellow</li>
-    <li>Brand Name: Aldo</li>
+    <li>الاسم: GRYDY Quilted Crossbody Bag</li>
+    <li>اللون: أصفر</li>
+    <li>اسم البراند: Aldo</li>
     {/* Add more list items here */}
   </ul>
   <div className="pt-4 border-t">
