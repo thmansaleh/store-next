e9a3bdd3d0788837bda6d6aa935d07e5d38637a5
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-20 15:46:05 +0400

    Update TamaraSteper.js

--- app/checkout/TamaraSteper.js
+++ app/checkout/TamaraSteper.js
@@ -6,7 +6,7 @@ return <ul className="relative flex flex-col md:flex-row gap-2   px-2">
   {/* Item */}
   <li className="md:shrink md:basis-0 flex-1 group flex gap-x-2 md:block">
     <div className="min-w-7 min-h-7 flex flex-col items-center md:w-full md:inline-flex md:flex-wrap md:flex-row text-xs align-middle">
-      <span className="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-gray-700 dark:text-white">
+      <span className="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-gray-700 dark:text-white h-7 w-7">
         1
       </span>
       <div className="mt-2 w-px h-full md:mt-0 md:ms-2 md:w-full md:h-px md:flex-1 bg-gray-200 group-last:hidden dark:bg-gray-700" />
@@ -24,7 +24,7 @@ return <ul className="relative flex flex-col md:flex-row gap-2   px-2">
   {/* Item */}
   <li className="md:shrink md:basis-0 flex-1 group flex gap-x-2 md:block">
     <div className="min-w-7 min-h-7 flex flex-col items-center md:w-full md:inline-flex md:flex-wrap md:flex-row text-xs align-middle">
-      <span className="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-gray-700 dark:text-white">
+      <span className="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-gray-700 dark:text-white h-7 w-7">
         2
       </span>
       <div className="mt-2 w-px h-full md:mt-0 md:ms-2 md:w-full md:h-px md:flex-1 bg-gray-200 group-last:hidden dark:bg-gray-700" />
@@ -42,7 +42,8 @@ return <ul className="relative flex flex-col md:flex-row gap-2   px-2">
   {/* Item */}
   <li className="md:shrink md:basis-0 flex-1 group flex gap-x-2 md:block">
     <div className="min-w-7 min-h-7 flex flex-col items-center md:w-full md:inline-flex md:flex-wrap md:flex-row text-xs align-middle">
-      <span className="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-gray-700 dark:text-white">
+      <span className="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-gray-700 dark:text-white
+h-7 w-7">
         3
       </span>
       <div className="mt-2 w-px h-full md:mt-0 md:ms-2 md:w-full md:h-px md:flex-1 bg-gray-200 group-last:hidden dark:bg-gray-700" />
