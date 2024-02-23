fdf28e0193316374c228e2cf14144faf985f16e0
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-23 14:06:40 +0400

    Update LanguageModal.js

--- app/account/LanguageModal.js
+++ app/account/LanguageModal.js
@@ -9,7 +9,7 @@ const OpenModal = () => {
   };
 
 const closeModal = () => {
-    inputRef.current.style.display='hidden'
+    inputRef.current.style.display='none'
   };
 
 
@@ -26,8 +26,8 @@ return <>
 
 
 
-<div onClick={closeModal} ref={inputRef} className="hidden opacity-75   transition-all modal-container flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 bottom-0 bg-gray-100 max-h-full">
-  <div id="div" className="   shadow relative rounded-lg w-3/4 bg-white transition-all">
+<div onClick={closeModal} ref={inputRef} className="hidden opacity-75 z-50  transition-all modal-container flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 bottom-0 bg-gray-100 max-h-full">
+  <div id="div" className=" z-50  shadow relative rounded-lg w-3/4 bg-white transition-all">
     <button onClick={closeModal} type="button" className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
       <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
