7bba2b5fecd85979d752614fdcb6586c9b8646bf
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-24 00:40:42 +0400

    Update LanguageModal.js

--- app/account/LanguageModal.js
+++ app/account/LanguageModal.js
@@ -37,7 +37,7 @@ return <>
       bg-red-2y00 justify-center">
       <div className="inline-flex items-center">
         <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlfor="html">
-          <input checked name="type" type="radio" className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-10" id="html" />
+          <input defaultChecked name="type" type="radio" className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-10" id="html" />
           <span className="absolute text-gray-900 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 16 16" fill="currentColor">
               <circle data-name="ellipse" cx={8} cy={8} r={8} />
