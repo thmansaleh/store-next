1c4f82ed834b6167b2fe7ef6ab62311db14b36e4
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-23 18:11:49 +0400

    Update CurrencyModal.js

--- app/account/CurrencyModal.js
+++ app/account/CurrencyModal.js
@@ -37,6 +37,9 @@ return <>
 
 <div ref={modal} className=" hidden bg-gray-100 bg-opacity-75 transition-all modal-container flex overflow-y-auto overflow-x-hidden fixed  z-40 inset-y-0 inset-x-0 justify-center items-center    bg-gray-400 max-h-full">
   <div id="div" className="z-50   shadow relative rounded-lg w-3/4 bg-white transition-all">
+
+
+
     <button onclick={closeModal} type="button" className=" text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 m-3 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
       <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
