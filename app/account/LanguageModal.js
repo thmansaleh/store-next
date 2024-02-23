17e91674f9852b2c4215410df013c13f79770450
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-23 14:46:43 +0400

    Update LanguageModal.js

--- app/account/LanguageModal.js
+++ app/account/LanguageModal.js
@@ -26,7 +26,7 @@ return <>
 
 
 
-<div onClick={closeModal} ref={inputRef} className="hidden opacity-75 z-50  transition-all modal-container flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 bottom-0 bg-gray-100 max-h-full">
+<div onClick={closeModal} ref={inputRef} className="hidden  z-50  transition-all modal-container flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 bottom-0  max-h-full">
   <div id="div" className=" z-50 opacity-100  shadow relative rounded-lg w-3/4 bg-white transition-all">
     <button onClick={closeModal} type="button" className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
       <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
