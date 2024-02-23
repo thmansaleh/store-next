480b5c43485471e9bb79a4003a07fef0861d8c7b
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-23 17:55:16 +0400

    Update CurrencyModal.js

--- app/account/CurrencyModal.js
+++ app/account/CurrencyModal.js
@@ -30,7 +30,14 @@ return <>
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
       </svg>
     </button>   
+
     <div className=" flex flex-col gap-1 p-2 font-sans text-base font-normal text-black">
+
+
+
+
+
+
       <div role="button" className="flex items-center w-full p-0 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
         <label htmlfor="vertical-list-react" className="flex items-center w-full px-3 py-2 cursor-pointer">
           <div className="grid mr-3 place-items-center">
@@ -45,49 +52,18 @@ return <>
               </label>
             </div>
           </div>
-          <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-400">
+          <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-400 px-2">
             درهم إماراتي
           </p>
         </label>
       </div>
-      <div role="button" className="flex items-center w-full p-0 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
-        <label htmlfor="vertical-list-vue" className="flex items-center w-full px-3 py-2 cursor-pointer">
-          <div className="grid mr-3 place-items-center">
-            <div className="inline-flex items-center">
-              <label className="relative flex items-center p-0 rounded-full cursor-pointer" htmlfor="vertical-list-vue">
-                <input name="vertical-list" id="vertical-list-vue" type="radio" className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-0" />
-                <span className="absolute text-gray-900 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 16 16" fill="currentColor">
-                    <circle data-name="ellipse" cx={8} cy={8} r={8} />
-                  </svg>
-                </span>
-              </label>
-            </div>
-          </div>
-          <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-400">
-            دولار أمريكي 
-          </p>
-        </label>
-      </div>
-      <div role="button" className="flex items-center w-full p-0 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
-        <label htmlfor="vertical-list-svelte" className="flex items-center w-full px-3 py-2 cursor-pointer">
-          <div className="grid mr-3 place-items-center">
-            <div className="inline-flex items-center">
-              <label className="relative flex items-center p-0 rounded-full cursor-pointer" htmlfor="vertical-list-svelte">
-                <input name="vertical-list" id="vertical-list-svelte" type="radio" className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-0" />
-                <span className="absolute text-gray-900 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 16 16" fill="currentColor">
-                    <circle data-name="ellipse" cx={8} cy={8} r={8} />
-                  </svg>
-                </span>
-              </label>
-            </div>
-          </div>
-          <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-400">
-            جنيه إسترليني 
-          </p>
-        </label>
-      </div>
+
+
+
+      
+
+
+
     </div>
   </div>
 </div>
