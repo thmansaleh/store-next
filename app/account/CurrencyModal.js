6a38b5d9cfdbbe2d8cbdcb4b312f04afbc1c8bae
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-23 18:27:58 +0400

    Update CurrencyModal.js

--- app/account/CurrencyModal.js
+++ app/account/CurrencyModal.js
@@ -22,7 +22,7 @@ const OpenModal = () => {
     modal.current.style.display='flex'
   };
 
-const closeModal = () => {
+const CloseModal = () => {
     modal.current.style.display='none'
 console.log("close")
   };
@@ -41,7 +41,7 @@ return <>
 
 
 
-    <button onclick={closeModal} type="button" className=" text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 m-3 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+    <button onclick={CloseModal} type="button" className=" text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 m-3 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
       <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
       </svg>
