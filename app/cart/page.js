27fc416fb4b63349367fdfef36d256fb248e4cc5
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-25 17:42:56 +0400

    Update page.js

--- app/cart/page.js
+++ app/cart/page.js
@@ -2,6 +2,7 @@ import SetNave from '../components/SetNave'
 import OrderDetailes from './OrderDetailes'
 import Item from './item/Item'
 import Link from "next/link"
+import PromotionCode from="./PromotionCode"
 export default function Cart(){
 
   return <div style={{"background-color":"#f7f7f7"}} className=" p-3">
@@ -47,6 +48,13 @@ my-2">
     <svg className="w-7 h-7  mx-2  fill-green-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g id="_01_align_center" data-name="01 align center"><path d="M18,0H14V1a2,2,0,0,1-4,0V0H6A3,3,0,0,0,3,3V24h7V23a2,2,0,0,1,4,0v1h7V3A3,3,0,0,0,18,0ZM15.874,22a4,4,0,0,0-7.748,0H5V17H8V15H5V3A1,1,0,0,1,6,2H8.126a4,4,0,0,0,7.748,0H18a1,1,0,0,1,1,1V15H16v2h3v5Z" /><rect x={10} y={15} width={4} height={2} /></g></svg>
     إضافة كوبون خصم
   </button>
+
+<PromotionCode/>
+
+
+
+
+
   <div className=" my-2 overflow-hidden rounded-xl border border-gray-300 bg-white px-4 py-6 md:mx-4">
     <div className="my-4 flex justify-between">
       <span className="text-gray-500 text-sm">المجموع الفرعي</span>
