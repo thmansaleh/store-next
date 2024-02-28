c27f963bf9de39406c0f1cd7bd5b1b0c78605f01
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-28 16:29:27 +0400

    Update PromotionCode.js

--- app/cart/PromotionCode.js
+++ app/cart/PromotionCode.js
@@ -2,7 +2,7 @@
 export default function PromotionCode() {
 
    return <> 
-<button  className="flex  bg-white rounded-lg text-sm font-semibold text-black justify-center items-center my-3 py-2 border-dashed border-2 w-full ">
+<button  className="flex  bg-white rounded-lg text-sm font-semibold text-black justify-center items-center my-3 py-2 border-dashed border-4 w-full ">
 
     <svg className="w-7 h-7  mx-2  fill-green-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g id="_01_align_center" data-name="01 align center"><path d="M18,0H14V1a2,2,0,0,1-4,0V0H6A3,3,0,0,0,3,3V24h7V23a2,2,0,0,1,4,0v1h7V3A3,3,0,0,0,18,0ZM15.874,22a4,4,0,0,0-7.748,0H5V17H8V15H5V3A1,1,0,0,1,6,2H8.126a4,4,0,0,0,7.748,0H18a1,1,0,0,1,1,1V15H16v2h3v5Z" /><rect x={10} y={15} width={4} height={2} /></g></svg>
     إضافة كوبون خصم
