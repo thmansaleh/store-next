b0779b00bfcae83917a4dcfa1a9df13283f3d976
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-28 19:07:46 +0400

    Update PromotionCode.js

--- app/cart/PromotionCode.js
+++ app/cart/PromotionCode.js
@@ -14,7 +14,7 @@ export default function PromotionCode() {
 
 
 
-<div  className="h-0 w-full  transition-all overflow-y-hidden">
+<div onClick={() => console.log(1)} className="h-20 w-full  transition-all overflow-y-hidden">
   <div className="mt-5 flex px-4">
     <input className="flex-1 rounded-s-xl border-2 border-black p-2 focus:outline-none" placeholder="ادخل الكود" type="text" defaultValue />
     <button className="flex w-20 justify-center rounded-e-xl bg-black p-2 text-white  hover:bg-opacity-80">تفعيل</button>
