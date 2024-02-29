59920a83c92ebd82d709ec872c1af3bf00d3e0c1
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-29 15:11:32 +0400

    Update PromotionCode.js

--- app/cart/PromotionCode.js
+++ app/cart/PromotionCode.js
@@ -20,7 +20,7 @@ inputRef.current.classList.toggle("h-20")
 
 <div ref={inputRef} className="h-0 w-full  transition-all overflow-y-hidden">
   <div className="mt-5 flex px-4">
-    <input className="flex-1 rounded-s-xl border-2 border-black p-2 focus:outline-none" placeholder="ادخل الكود" type="text" defaultValue />
+    <input className="flex-1 rounded-s-xl border-2 border-black p-2 focus:outline-none" placeholder="ادخل الكود" type="text"  />
     <button className="flex w-20 justify-center rounded-e-xl bg-black p-2 text-white  hover:bg-opacity-80">تفعيل</button>
   </div>
 </div>
