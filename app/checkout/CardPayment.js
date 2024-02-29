073d8beda2e998ffac92bb1479b2f4fe35337c4f
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-01 01:45:12 +0400

    Update CardPayment.js

--- app/checkout/CardPayment.js
+++ app/checkout/CardPayment.js
@@ -1,12 +1,24 @@
+"use client'
+import { useDispatch } from "react-redux";
+import { changePaymentMethod} from "@/app/store/features/checkout";
 
 import CardForm from "./CardForm"
 
 export default function CardPayment() {
+    const dispatch = useDispatch()
+
 
 return   <div className>
     <div className="inline-flex w-full  py-2 items-center">
       <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlfor="custom-style1">
-        <input name="type" type="radio" className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-gray-900/10 bg-gray-900/5 p-0 text-gray-900 transition-all before:absolute before:top-2/4 
+        <input 
+
+onClick={() =>{
+      dispatch(changePaymentMethod("card")) 
+
+}}
+
+ name="type" type="radio" className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-gray-900/10 bg-gray-900/5 p-0 text-gray-900 transition-all before:absolute before:top-2/4 
   before:left-2/4 
   before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-0" id="custom-style1" />
         <span className="absolute text-gray-900 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
