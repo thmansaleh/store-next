2ee05c0029f2abefc32f1dc0099827da1cf631f1
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-01 01:17:41 +0400

    Update store.js

--- app/store/store.js
+++ app/store/store.js
@@ -1,6 +1,7 @@
 "use client"
 import { configureStore } from '@reduxjs/toolkit';
 import navigationReducer from './features/navigationSlice';
+import checkoutReducer from './features/checkout'
 
 const store = configureStore({
     reducer: {
