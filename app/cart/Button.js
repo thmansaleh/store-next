12b63fe04f5809ef00d5acf425a31357f8658dfe
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-29 14:06:32 +0400

    Update Button.js

--- app/cart/Button.js
+++ app/cart/Button.js
@@ -1,5 +1,5 @@
 'use client'
-export default function PromotionCode() {
+export default function Button() {
 
    return <> 
 <button onclick={() => console.log("button")} className="flex  bg-white rounded-lg text-sm font-semibold text-black justify-center items-center my-3 py-2 border-dashed border-6 w-full ">
