26f544d1bb6877a923a7c25721829580a42a28d1
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-02-29 14:29:50 +0400

    Update Button.js

--- app/cart/Button.js
+++ app/cart/Button.js
@@ -1,7 +1,7 @@
 'use client'
 export default function Button() {
 
-   return <div onclick={() => console.log("button")} className=" bg-red-700 rounded-lg text-sm font-semibold text-black   my-3 py-2 border-dashed border-6 w-full ">
+   return <div onClick={() => console.log("button")} className=" bg-red-700 rounded-lg text-sm font-semibold text-black   my-3 py-2 border-dashed border-6 w-full ">
 
     
     إضافة كوبون خصم
