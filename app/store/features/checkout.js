2431f4b1ff596cc02af87f016ae49d75b1241c82
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-01 01:14:07 +0400

    Update checkout.js

--- app/store/features/checkout.js
+++ app/store/features/checkout.js
@@ -1,1 +1,25 @@
-h
+"use client"
+import { createSlice } from '@reduxjs/toolkit';
+
+const checkoutSlice= createSlice({
+  name: 'checkout',
+  initialState: {
+paymentMethod:'',
+name:"",
+cardHolderName:"",
+cardNumber:0,
+ccv:0,
+
+
+},
+  reducers: {
+    changePaymentMethod: (state, action) => {
+      state.paymentMethod=action.payload;
+    }
+
+  },
+});
+
+export const { changePaymentMethod } = checkoutSlice.actions;
+
+export default checkoutSlice.reducer;
