e2c186c5b04914f1d71005e3466b01a91d81c807
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-02 20:57:40 +0400

    Update Home.js

--- app/home/Home.js
+++ app/home/Home.js
@@ -3,14 +3,14 @@ import SetNave from '@/app//components/SetNave'
 import SearchInput from "@/app/components/SearchInput"
 import FristSlider from './FristSlider'
 import Products from'@/app/home/Products'
-export default function Home(){
+export default function Home({products}){
   return <>
     <SetNave name='/' show={true}/>
 <SearchInput/>
 <FristSlider/>
 
 <div className="p-2 flex wrap justify-around bg-white  flex-wrap">
-<Products/>
+<Products products={products}/>
 </div>
 
 
