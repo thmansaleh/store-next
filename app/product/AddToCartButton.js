f027eafda817ce2201e0d7894f1d355fd439d5e0
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-03 03:12:36 +0400

    Update AddToCartButton.js

--- app/product/AddToCartButton.js
+++ app/product/AddToCartButton.js
@@ -5,7 +5,7 @@ import { addItemToCart} from "@/app/store/features/cart";
 export default function AddToCartButton({item}){
     const dispatch = useDispatch()
 
-return <div onClick={dispatch(addItemToCart(item))}
+return <div onClick={() => dispatch(addItemToCart(item))}
  className="w-full bg-white select-none p-3 sticky bottom-0">
   <button className="rounded-lg w-full bg-black text-sm font-semibold text-white py-3">إضافة الى السلة</button>
 </div>
