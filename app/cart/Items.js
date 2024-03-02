b9ed5aec312cec350b5e9fea30ff4ae3547336ca
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-02 16:44:38 +0400

    Update Items.js

--- app/cart/Items.js
+++ app/cart/Items.js
@@ -1,7 +1,33 @@
 "use client"
 
 export default function Items(){
- return <>
+ return <div>
+ 
+
+
+
+    <div className="my-1.5 rounded-lg   border w-full flex border-gray-300 justify-between items-center bg-white p-3">
+      <img className="object-contain w-24 mx-2" loading="lazy" src="https://jomla.ae/_next/image/?url=https%3A%2F%2Fwp.jomla.ae%2Fwp-content%2Fuploads%2F2021%2F12%2FN42661645A_1.jpg&w=256&q=75" />
+      <div className>
+        <h3 className="text-sm line-clamp-2">كارت درفت للأطفال Pedal Drift Scooter - Cool Baby</h3>
+        <span className="text-md my-2 inline-block font-semibold">1230 AED</span>
+        <div className="b flex justify-between items-center  ">
+          <div>
+            <button className="h-8 w-8 rounded-full border border text-xl leading-none text-black hover:bg-black hover:text-white">+</button>
+            <input className="mx-1 h-8 w-14 appearance-none rounded-2xl border border-gray-600 text-center outline-none" type="number" max={20} min={1} defaultValue={1} /><button className="h-8 w-8 rounded-full border border text-xl leading-none text-primary hover:bg-black hover:text-white">-</button>
+          </div>
+          <svg className="fill-red-500          h-5 w-5" stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 16 16" height={20} width={20} xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z" />
+            <path fillRule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clipRule="evenodd" />
+          </svg>
+        </div>
+      </div>
+    </div>
+
+
+
+
+</div>
 
 
 }
