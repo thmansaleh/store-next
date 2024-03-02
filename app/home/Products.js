04eb42d1e0baf195b9f194cd6a0ee6ef9eef64d0
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-02 20:45:12 +0400

    Update Products.js

--- app/home/Products.js
+++ app/home/Products.js
@@ -7,8 +7,7 @@ const products = async () => {
     return products.products
     
     }
-return products. map((e,i) => {
-return <Link href="product/hthhh" className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
+return products. map((e,i) => { <Link href="product/hthhh" className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
     <span className=" bg-red-700 text-white top-0 right-0 text-center absolute rounded-bl-lg px-3 text-xs bg-blend-multiply line-height opacity-80 ">خصم 20%</span>
     <div className="  ">
       <img className="h-48 w-full object-contain " src="https://jomla.ae/_next/image/?url=https%3A%2F%2Fwp.jomla.ae%2Fwp-content%2Fuploads%2F2022%2F11%2F71LinfGHuL._AC_SX466_.jpg&w=750&q=75" alt />
