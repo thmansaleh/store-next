cbba470604445ab38eebb91f5f0d1094d7a9aea2
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-02 23:56:43 +0400

    Update Products.js

--- app/home/Products.js
+++ app/home/Products.js
@@ -4,7 +4,8 @@ export default function Products(){
 
 
 
-return [1,2,2,2,2,2,2,2,2].map((e,i) => { <Link href="product/hthhh" className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
+return [1,2,2,2,2,2,2,2,2].map((e,i) => {
+return <Link href="product/hthhh" className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
     <span className=" bg-red-700 text-white top-0 right-0 text-center absolute rounded-bl-lg px-3 text-xs bg-blend-multiply line-height opacity-80 ">خصم 20%</span>
     <div className="  ">
       <img className="h-48 w-full object-contain " src="https://jomla.ae/_next/image/?url=https%3A%2F%2Fwp.jomla.ae%2Fwp-content%2Fuploads%2F2022%2F11%2F71LinfGHuL._AC_SX466_.jpg&w=750&q=75" alt />
