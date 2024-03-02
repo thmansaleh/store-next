97f1d27567a6434d99a7fd962d88b167c01ebc7b
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-03 01:49:25 +0400

    Update Products.js

--- app/home/Products.js
+++ app/home/Products.js
@@ -1,10 +1,8 @@
-"use client"
+
 import Link from 'next/link'
 export default function Products({products}){
 
-console.log(products)
-
-return [1,2,2,2,2,2,2,2,2].map((e,i) => {
+return products.map((e,i) => {
 return <Link href="product/hthhh" className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
     <span className=" bg-red-700 text-white top-0 right-0 text-center absolute rounded-bl-lg px-3 text-xs bg-blend-multiply line-height opacity-80 ">خصم 20%</span>
     <div className="  ">
