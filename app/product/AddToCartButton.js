9f56d9145c1924f754eb09060ca5dcbd5aab0295
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-03 02:37:04 +0400

    Update AddToCartButton.js

--- app/product/AddToCartButton.js
+++ app/product/AddToCartButton.js
@@ -1,6 +1,12 @@
-export default function AddToCartButton(){
 
-return <div className="w-full bg-white select-none p-3 sticky bottom-0">
+import { useDispatch } from "react-redux";
+import { addItemToCart} from "@/app/store/features/cart";
+
+export default function AddToCartButton({item}){
+    const dispatch = useDispatch()
+
+return <div onClick={dispatch(addItemToCart(item))}
+ className="w-full bg-white select-none p-3 sticky bottom-0">
   <button className="rounded-lg w-full bg-black text-sm font-semibold text-white py-3">إضافة الى السلة</button>
 </div>
 
