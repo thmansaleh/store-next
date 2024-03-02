19ade5932119a8e2a17b09595f62104f17475948
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-03 01:46:39 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,14 +3,14 @@
 
 import Home from '@/app/home/Home'
 
-const products= async () => {
+const productss= async () => {
      const data =   await fetch('https://dummyjson.com/products')
     const products = await data.json()
     return products.products
 
     }
 export default async function Page() {
-const products = await products()
+const products = await productss()
 // console.log(products)
   return  <Home products={products}/>
 }
