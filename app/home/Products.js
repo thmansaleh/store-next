336ea845e34dfa4603b83a2ca4b9ea872af52890
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-03 16:03:21 +0400

    Update Products.js

--- app/home/Products.js
+++ app/home/Products.js
@@ -3,7 +3,8 @@ import Link from 'next/link'
 export default function Products({products}){
 
 return products.map((e,i) => {
-return <Link href="product" className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
+const url =`product/${e.id}`
+return <Link href={url} className=" relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
     <span className=" bg-red-700 text-white top-0 right-0 text-center absolute rounded-bl-lg px-3 text-xs bg-blend-multiply line-height opacity-80 ">خصم 20%</span>
     <div className="  ">
       <img className="h-48 w-full object-contain " src={e.thumbnail} alt />
