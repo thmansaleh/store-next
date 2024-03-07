746bdab7fcc9d85e0db41cebbe93ff9d3a46ff45
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-07 17:48:06 +0400

    Update Items.js

--- app/cart/Items.js
+++ app/cart/Items.js
@@ -11,9 +11,9 @@ const itemContainer =useRef(null)
     const dispatch = useDispatch()
 
 const remove =() =>{
+itemContainer.classList.add("scale-0")
 setTimeout(() => {
 
-containerEffect()
 dispatch(removeItemFromCart(id))
 }, 1000);
 
@@ -21,9 +21,7 @@ dispatch(removeItemFromCart(id))
 
 
 
-const containerEffect =() =>{
-itemContainer.classList.add("scale-0")
-}
+
 
  return <div>
 
@@ -40,7 +38,28 @@ return <div ref={itemContainer} key={i} className=" transition-all  my-1.5 round
             <input className="mx-1 h-8 w-14 appearance-none rounded-2xl border border-gray-600 text-center outline-none" type="number" max={20} min={1} defaultValue={1} /><button className="h-8 w-8 rounded-full border border text-xl leading-none text-primary hover:bg-black hover:text-white">-</button>
           </div>
           
-<DeleteButton id={e.id} containerEffect={containerEffect}/>
+
+
+
+
+
+
+
+
+
+
+ <svg onClick={remove} className="fill-red-500          h-5 w-5" stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 16 16" height={20} width={20} xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z" />
+            <path fillRule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clipRule="evenodd" />
+          </svg>
+
+
+
+
+
+
+
+
 
 
 
