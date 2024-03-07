00248b06e22d8fa650d0d2acd829613201af8ab6
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-07 17:30:41 +0400

    Update Items.js

--- app/cart/Items.js
+++ app/cart/Items.js
@@ -1,14 +1,20 @@
 "use client"
+import {useRef} from "react"
+
 import DeleteButton from "./DeleteButton"
 export default function Items({cartItems}){
 
     const cartItemss= cartItems 
+const itemContainer =useRef(null)
+const containerEffect =() =>{
+itemContainer.classList.add("scale-0")
+}
 
  return <div>
 
   {cartItemss.map((e,i) => { 
 
-return <div key={i} className="my-1.5 rounded-lg   border w-full flex border-gray-300 justify-between items-center bg-white p-3">
+return <div ref={itemContainer} key={i} className=" transition-all  my-1.5 rounded-lg   border w-full flex border-gray-300 justify-between items-center bg-white p-3">
       <img className="object-contain w-24 mx-2" loading="lazy" src={e.thumbnail} />
       <div className>
         <h3 className="text-sm line-clamp-2">{e.description}</h3>
@@ -19,7 +25,7 @@ return <div key={i} className="my-1.5 rounded-lg   border w-full flex border-gra
             <input className="mx-1 h-8 w-14 appearance-none rounded-2xl border border-gray-600 text-center outline-none" type="number" max={20} min={1} defaultValue={1} /><button className="h-8 w-8 rounded-full border border text-xl leading-none text-primary hover:bg-black hover:text-white">-</button>
           </div>
           
-<DeleteButton id={e.id} />
+<DeleteButton id={e.id} containerEffect={containerEffect}/>
 
 
 
