5d0b3e34412cd5e094a68fc172202cdb8a585dcc
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-08 02:37:56 +0400

    Update page.js

--- app/cart/page.js
+++ app/cart/page.js
@@ -17,7 +17,7 @@ if(cartItems.length>0){
 <SetNave name='cart' show={false}/>
 <Items cartItems={cartItems}/>
 <PromotionCode/>
-<OrderDetailes/>
+<OrderDetailes items{cartItems}/>
 </div>
 </div>
 }else{
