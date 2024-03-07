4973b1d8b938a491a98427fa6e84636a82928803
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-07 15:55:50 +0400

    Update cart.js

--- app/store/features/cart.js
+++ app/store/features/cart.js
@@ -4,7 +4,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const cartSlice= createSlice({
   name: 'cart',
   initialState: {
-items:[1]
+items:[]
 },
   reducers: {
     addItemToCart: (state, action) => {
