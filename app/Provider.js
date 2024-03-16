2848e0480d89345009e83280743371256093d6aa
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-16 15:13:09 +0400

    Update Provider.js

--- app/Provider.js
+++ app/Provider.js
@@ -2,10 +2,13 @@
 import { Provider } from "react-redux"
 import store from "./store/store"
 import Navigation from './components/navigation/Navigation'
+import NavigationBar from "./components/NavigationBar"
 
 function ProviderPage({children}) {
   return <Provider store={store}>{children}
-<Navigation/>
+
+<NavigationBar/>
+
 </Provider>
     
   
