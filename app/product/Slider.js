a0565e774c82372c7751aaecc28441b29543f3b3
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-18 02:58:46 +0400

    Update Slider.js

--- app/product/Slider.js
+++ app/product/Slider.js
@@ -26,7 +26,7 @@ navigation Scrollbar pagination={{ clickable: true }}
 
 {[1,4,4,4,4].map(e=>{
 
-<SwiperSlide style={{ textAlign: 'center', height: '50vh', width: '100%', margin: 0 }}>
+return <SwiperSlide style={{ textAlign: 'center', height: '50vh', width: '100%', margin: 0 }}>
   
     <img className="w-100 product-img-slider" src='https://media.cnn.com/api/v1/images/stellar/prod/170321162454-dubai-police-bugatti.jpg?q=h_619,w_1024,x_0,y_31,c_crop/h_619,w_1100,c_lpad,b_rgb:061015/h_720,w_1280' alt="My Image" />
   </SwiperSlide>
