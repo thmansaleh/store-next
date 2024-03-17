956a90c4a6c88e4509ea016b3ba16b34a33e122b
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-18 02:56:47 +0400

    Update Slider.js

--- app/product/Slider.js
+++ app/product/Slider.js
@@ -3,7 +3,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import './slider.css'
-import SwiperIcons from './SwiperIcons'
 import 'swiper/css';
 import 'swiper/css/autoplay'
 import 'swiper/css/navigation';
@@ -21,19 +20,29 @@ slidesPerView={1}
 navigation Scrollbar pagination={{ clickable: true }}
 
 >
- <SwiperSlide style={{ textAlign: 'center', height: '50vh', width: '100%', margin: 0 }}>
+
+
+
+
+{[1,4,4,4,4].map(e=>{
+
+<SwiperSlide style={{ textAlign: 'center', height: '50vh', width: '100%', margin: 0 }}>
   
     <img className="w-100 product-img-slider" src='https://media.cnn.com/api/v1/images/stellar/prod/170321162454-dubai-police-bugatti.jpg?q=h_619,w_1024,x_0,y_31,c_crop/h_619,w_1100,c_lpad,b_rgb:061015/h_720,w_1280' alt="My Image" />
   </SwiperSlide>
- <SwiperSlide style={{ textAlign: 'center', height: '50vh', width: '100%', margin: 0 }}>
-  
-    <img className="w-100 product-img-slider" src='https://media.cnn.com/api/v1/images/stellar/prod/170321162454-dubai-police-bugatti.jpg?q=h_619,w_1024,x_0,y_31,c_crop/h_619,w_1100,c_lpad,b_rgb:061015/h_720,w_1280' alt="My Image" />
-  </SwiperSlide>
- <SwiperSlide style={{ textAlign: 'center', height: '50vh', width: '100%', margin: 0 }}>
-  
-    <img className="w-100 product-img-slider" src='https://media.cnn.com/api/v1/images/stellar/prod/170321162454-dubai-police-bugatti.jpg?q=h_619,w_1024,x_0,y_31,c_crop/h_619,w_1100,c_lpad,b_rgb:061015/h_720,w_1280' alt="My Image" />
-  </SwiperSlide>
-<SwiperIcons/>
+
+
+})}
+
+
+
+
+
+
+
+
+
+
 </Swiper>
 
 </>
