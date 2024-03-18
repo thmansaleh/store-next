e76afae6b2847352d8433f6bd296aecf4259f14c
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-18 20:38:07 +0400

    Update AddToCartButton.js

--- app/product/AddToCartButton.js
+++ app/product/AddToCartButton.js
@@ -7,7 +7,7 @@ export default function AddToCartButton({item}){
 
 return <div onClick={() => dispatch(addItemToCart(item))}
  className="w-full bg-white select-none p-3 sticky bottom-0">
-  <button className="focus:outline-none  rounded-lg w-full bg-black text-sm font-semibold text-white py-3 hover:bg-transparent active:bg-transparent">إضافة الى السلة</button>
+  <button className="focus:outline-none  rounded-lg w-full bg-black text-sm font-semibold text-white py-3  ">إضافة الى السلة</button>
 </div>
 
 }
