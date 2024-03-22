6d4261165833c7623d90db9d8d891988e06f1ddc
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-22 20:29:37 +0400

    Update SimilarProducts.js

--- app/product/SimilarProducts.js
+++ app/product/SimilarProducts.js
@@ -49,7 +49,7 @@ return <SwiperSlide className="my-8">
   <img className="w-full h-40 object-contain " src="https://www.sephora.ae/dw/image/v2/BCZG_PRD/on/demandware.static/-/Sites-masterCatalog_Sephora/default/dw8bfb7a5e/images/hi-res/SKU/SKU_2487/553961_swatch.jpg?sw=265&sh=265&sm=fit" alt />
   <span className=" line-clamp-2 text-sm my-2 px-2">SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...</span>
   <div className="p-2 space-x-px text-sm bg-zinc-100  "><span>899</span>
-    <span className="text-green-600">د.أ</span></div>
+    <span className="text-green-600 mx-px">د.أ</span></div>
   <span className=" bg-red-700 text-white top-0  text-center  absolute rounded-br-lg  rtl:rounded-bl-lg rtl:rounded-br-none  px-3 text-xs bg-blend-multiply line-height start-0 opacity-80 ">خصم 20%</span>
 </Link>
 
