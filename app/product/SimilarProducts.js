7283eddd157745593e3a66b2fa7dcff042b94ebc
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-22 16:06:37 +0400

    Update SimilarProducts.js

--- app/product/SimilarProducts.js
+++ app/product/SimilarProducts.js
@@ -1,7 +1,7 @@
 "use client"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
+import Link from 'next/link'
 import './slider.css'
 import 'swiper/css';
 import 'swiper/css/autoplay'
@@ -44,13 +44,16 @@ slidesPerView={2}
 
 return <SwiperSlide className="">
 
-    <a href="#" className=" relative overflow-hidden shadow-lg rounded-lg inline-block w-44">
+    <Link href={{
+    pathname: '/product',
+    query: { product_id: e.id},
+  }} className=" relative overflow-hidden shadow-lg rounded-lg inline-block w-44">
   <img className="w-full h-48 object-contain " src="https://www.sephora.ae/dw/image/v2/BCZG_PRD/on/demandware.static/-/Sites-masterCatalog_Sephora/default/dw8bfb7a5e/images/hi-res/SKU/SKU_2487/553961_swatch.jpg?sw=265&sh=265&sm=fit" alt />
   <span className=" line-clamp-2 text-sm my-2 px-2">SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...</span>
   <div className="p-2 space-x-px text-sm bg-zinc-100  "><span>899</span>
     <span className="text-green-600">د.أ</span></div>
   <span className=" bg-red-700 text-white top-0  text-center  absolute rounded-br-lg  rtl:rounded-bl-lg rtl:rounded-br-none  px-3 text-xs bg-blend-multiply line-height start-0 opacity-80 ">خصم 20%</span>
-</a>
+</Link>
 
 
 
