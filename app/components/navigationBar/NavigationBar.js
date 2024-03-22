495dfaca67a4ddd7b2466dafd1d299476fbc5013
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-23 01:15:28 +0400

    Update NavigationBar.js

--- app/components/navigationBar/NavigationBar.js
+++ app/components/navigationBar/NavigationBar.js
@@ -11,9 +11,9 @@ export default function NavigationBar(){
 return  <div id="navs" className=" nav fixegd rounded-t-lg bottom-0  w-screen py-4 bg-white  flex items-center  justify-around ">
   
   {icons.map((e,i)=>{
- return  <div className="flex items-center justify-center bg-grvay-500 px-4 h-10   overflow-hidden   relative">
+ return  <div className="flex items-center justify-center bg-grvay-500  h-10   overflow-hidden   relative">
     <input type="radio" id={e.page} className="navigationInput" name="nav"/>
-    <label  for={e.page}>
+    <label className="px-4"  for={e.page}>
     
 {e.icon}
       <span className="bg-black  -bottom-2 inset-x-1/2    translate-x-1/2 absolute  h-1.5 w-1.5  rounded-full  transition-all duration-150"></span>
