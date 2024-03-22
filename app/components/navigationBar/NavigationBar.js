0cff2ed7338344e122984f1f5b3870bfd8be6dcf
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-23 00:53:32 +0400

    Update NavigationBar.js

--- app/components/navigationBar/NavigationBar.js
+++ app/components/navigationBar/NavigationBar.js
@@ -11,8 +11,8 @@ export default function NavigationBar(){
 return  <div id="navs" className=" nav fixegd rounded-t-lg bottom-0  w-screen py-4 bg-white  flex items-center  justify-around ">
   
   {icons.map((e,i)=>{
- return  <div className="navs flex items-center justify-center bg-grvay-500 px-4 h-10   overflow-hidden   relative">
-    <input type="radio" id={e.page} className={styles.navigationInput} name="nav"/>
+ return  <div className="flex items-center justify-center bg-grvay-500 px-4 h-10   overflow-hidden   relative">
+    <input type="radio" id={e.page} className="navigationInput" name="nav"/>
     <label  for={e.page}>
     
 {e.icon}
