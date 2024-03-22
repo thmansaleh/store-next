cf1d84aca0a4890ac39862f3bd444bd864c23a97
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-23 02:21:43 +0400

    Update NavigationBar.js

--- app/components/navigationBar/NavigationBar.js
+++ app/components/navigationBar/NavigationBar.js
@@ -6,7 +6,7 @@ import {icons} from "./NavigationIcons"
 export default function NavigationBar(){
 const router = useRouter()
 const navigation = (page)=>{
-router.push(`/${page}`)
+router.push('/cart')
 }
 
 
@@ -14,7 +14,9 @@ router.push(`/${page}`)
 return  <div  className=" border-solid border-t border-inherit  fixegd rounded-t-lg bottom-0  w-screen py-4 bg-white  flex items-center  justify-around ">
   
   {icons.map((e,i)=>{
- return  <div onClick={navigation(e.page)}
+ return  <div onClick={()=>{
+router.push(`/${e.page}`)
+}}
 className="flex items-center justify-center bg-grvay-500  h-10   overflow-hidden   relative">
     <input type="radio" id={e.page} className="navigationInput" name="nav"/>
     <label className="px-4"  for={e.page}>
