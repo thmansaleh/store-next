14720438826ec2e1e7b4b13d3042893b6a9424e3
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-22 21:33:16 +0400

    Update SimilarProducts.js

--- app/product/SimilarProducts.js
+++ app/product/SimilarProducts.js
@@ -59,7 +59,7 @@ return <SwiperSlide className="my-8">
   <img className="w-full h-40 object-contain " src={e} alt />
   <span className=" line-clamp-2 text-sm my-2 px-2">SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...</span>
   <div className="p-2 space-x-px text-sm bg-zinc-100  "><span>899</span>
-    <span className="text-green-600 mx-px">د.أ</span></div>
+    <span className=" inline-block text-green-600 mx-px">د.أ</span></div>
   <span className=" bg-red-700 text-white top-0  text-center  absolute rounded-br-lg  rtl:rounded-bl-lg rtl:rounded-br-none  px-3 text-xs bg-blend-multiply line-height start-0 opacity-80 ">خصم 20%</span>
 </Link>
 
