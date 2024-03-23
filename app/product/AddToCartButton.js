89221f1060c7add5b1ee981cfd93699c93a5508e
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-23 20:42:07 +0400

    Update AddToCartButton.js

--- app/product/AddToCartButton.js
+++ app/product/AddToCartButton.js
@@ -8,13 +8,29 @@ import SimilarProducts from"@/app/product/SimilarProducts"
 
 export default function AddToCartButton({item}){
     const dispatch = useDispatch()
+  const [open, setOpen] = useState(false)
+
 
 return <>
 
- <div onClick={() => dispatch(addItemToCart(item))}
+ <div onClick={() =>{ 
+dispatch(addItemToCart(item)
+setOpen(true)
+}
+
+)}
  className="w-full bg-white select-none p-3 sticky bottom-0">
-  <div className="focus:outline-none  rounded-lg w-full bg-black text-sm font-semibold text-white py-3  ">إضافة الى السلة</div>
+  <div className="focus:outline-none  rounded-lg w-full bg-black text-sm font-semibold text-white py-3 text-center  ">إضافة الى السلة</div>
 </div>
+
+      <BottomSheet
+onDismiss={()=>setOpen(false)}
+header={<h1 className="text-md font-semibold ">منتجات مشابهه</h1>}
+ open={open}>
+
+<SimilarProducts/>
+
+</BottomSheet>
 </>
 
 }
