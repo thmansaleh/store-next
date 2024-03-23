387c548b9be8d631a5f2d2b594b037e5d3f7f703
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-23 16:33:59 +0400

    Update Category.js

--- app/categories/Category.js
+++ app/categories/Category.js
@@ -1,30 +1,10 @@
 
  export default function Categories(){ 
   
-   return <div className="w-36 select-none bg-gray-200  overflow-auto">
-  <div className="w-full py-3  text-sm text-black px-2 font-semibold  bg-black  text-white "> عبايات</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> ملابس داخلية</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">رجالي</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">ملابس أطفال</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">إلكترونيات</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> سماعات</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> أحذية</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">هواتف ذكية</div>
-  <div className="w-full py-3  text-sm text-black px-2 font-semibold   "> عبايات</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold hover:border-b-2 border-gray-900"> ملابس داخلية</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">رجالي</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">ملابس أطفال</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">إلكترونيات</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> سماعات</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> أحذية</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">هواتف ذكية</div>
-  <div className="w-full py-3  text-sm text-black px-2 font-semibold   "> عبايات</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold hover:border-b-2 border-gray-900"> ملابس داخلية</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">رجالي</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">ملابس أطفال</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">إلكترونيات</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> سماعات</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold "> أحذية</div>
-  <div className="w-full py-3 px-2  text-sm text-black   font-semibold ">هواتف ذكية</div>
+   return <div>      
+  <h1 className="block text-sm font-semibold p-2">تسوق حسب الفئة</h1>
+  <div className="flex justify-around gap-4 bg-white flex-wrap py-2 ">
+    <div className=" shadow-lg rounded-lg text-center"><img className="w-16 h-16 inline-block object-contain" src="https://alkalej.com/cdn/shop/files/PhotoRoom_047_20230813_024400.jpg?v=1693780269&width=1000" alt="qqq" /><span className="text-xs p-2 text-gray-700 block font-semibold ">عبايات ملونة</span></div>
+  </div>
 </div>
  }
