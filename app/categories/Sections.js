798d7f365a50ac90a0753e5c44fc212a364d8d69
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-23 16:37:24 +0400

    Update Sections.js

--- app/categories/Sections.js
+++ app/categories/Sections.js
@@ -16,17 +16,9 @@ const categories = [
   "Tools & Home Improvement",
   "Office Supplies"
 ];
-  return    <div>
-
-      {categories.map(e=>{
-return <div class=" bg-gray-100  ">
-        <input class="hidden categoryInput" type="radio" id={e} name="category">
-        <label class="bg-red-200 w-full p-2 block" for={e}>
-        </label>
-      </div>
-})}
+  
 
 
-   </div>
+
 
 }
