a6be47b43c1650308831ab6551d2565613137767
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-23 20:36:58 +0400

    Update BottomSheet.js

--- app/categories/BottomSheet.js
+++ app/categories/BottomSheet.js
@@ -1,9 +1,6 @@
 "use client"
 import { useState } from 'react'
 import { BottomSheet } from 'react-spring-bottom-sheet'
-
-// if setting up the CSS is tricky, you can add this to your page somewhere:
-// <link rel="stylesheet" href="https://unpkg.com/react-spring-bottom-sheet/dist/style.css" crossorigin="anonymous">
 import 'react-spring-bottom-sheet/dist/style.css'
 import SimilarProducts from"@/app/product/SimilarProducts"
 export default function BottomSheett() {
