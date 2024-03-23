29da0304eb8db1d4cdf9abb5a53fc80828def21c
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-23 20:38:49 +0400

    Update AddToCartButton.js

--- app/product/AddToCartButton.js
+++ app/product/AddToCartButton.js
@@ -9,9 +9,12 @@ import SimilarProducts from"@/app/product/SimilarProducts"
 export default function AddToCartButton({item}){
     const dispatch = useDispatch()
 
-return <div onClick={() => dispatch(addItemToCart(item))}
+return <>
+
+ <div onClick={() => dispatch(addItemToCart(item))}
  className="w-full bg-white select-none p-3 sticky bottom-0">
   <div className="focus:outline-none  rounded-lg w-full bg-black text-sm font-semibold text-white py-3  ">إضافة الى السلة</div>
 </div>
+</>
 
 }
