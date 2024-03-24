634dec9978b4a869b84f16892fa59c7ff6400c85
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-24 17:06:29 +0400

    Update SuccessfullyContent.js

--- app/product/addToCartBtnContent/SuccessfullyContent.js
+++ app/product/addToCartBtnContent/SuccessfullyContent.js
@@ -1,10 +1,17 @@
 "use client"
+import Image from 'next/image'
 import Link from 'next/link'
 export default function({img}){
 
 return <div className="text-center py-3
 flex items-center flex-col gap-4">
-  <img className="shadow-lg  object-cover w-24 h-24 rounded-lg inline-block" src={img} alt />
+  <Image
+width={90}
+      height={90}
+      alt="cart"
+style={{objectFit: "cover",borderRadius:"10px"}}
+
+  src={img}  />
   <span className="circle flex items-center  justify-center h-5 w-5 transition-all  inline-block  rounded-full bg-green-400">
     <svg xmlns="http://www.w3.org/2000/svg" className=" stroke-inherit transition-all size-4 fill-white" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" strokeWidth={1}>
       <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
