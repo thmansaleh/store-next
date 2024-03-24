c460c46e8564fe23f59dae0b20e848ac95afaf03
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-24 20:22:44 +0400

    Update NavigationBar.js

--- app/components/navigationBar/NavigationBar.js
+++ app/components/navigationBar/NavigationBar.js
@@ -29,6 +29,9 @@ return  <div ref={navigationRef}  className=" z-1 fixed border-solid border-t bo
  return  <div onClick={()=>{
 router.push(`/${e.page}`)
 }}
+{e.alert&&<span className="bg-red-500 flex items-center justify-center w-2 absolute top-0 left-1 scale-75 transition-all  h-2 p-2 text-white  text-xs rounded-full ">
+  2
+</span>}
 className="flex items-center justify-center bg-grvay-500  h-10   overflow-hidden   relative">
     <input type="radio" id={e.page} className="navigationInput" name="nav"/>
     <label className="px-4"  for={e.page}>
