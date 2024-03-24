06e274b443d296cc01b0d854ac426bef4b994def
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-24 20:49:19 +0400

    Update NavigationBar.js

--- app/components/navigationBar/NavigationBar.js
+++ app/components/navigationBar/NavigationBar.js
@@ -5,21 +5,16 @@ import {useRef ,useEffect} from "react"
  import { useRouter } from 'next/navigation'
 import {icons} from "./NavigationIcons"
 export default function NavigationBar(){
-    const display = useSelector(state => state.navigation.display);
+    const alert = useSelector(state => state.cart.alert);
+const cartItems = useSelector(state => state.cart.items);
+
 const router = useRouter()
 const navigationRef =useRef(null)
 const navigation = (page)=>{
 router.push('/cart')
 }
 
-useEffect(()=>{
-if(display){
-navigationRef. current.style.display="flex"
-}else{
-navigationRef. current.style.display="none"
-}
 
-},[display])
 
 
 
@@ -31,8 +26,8 @@ router.push(`/${e.page}`)
 }}
 
 className="flex items-center justify-center bg-grvay-500  h-10   overflow-hidden   relative">
-{e.page=="cart"&&  <span class="bg-red-500 flex items-center justify-center w-2 absolute top-0 left-1 scale-75 transition-all  h-2 p-2 text-white  text-xs rounded-full ">
-      2
+{alert&&  <span class="bg-red-500 flex items-center justify-center w-2 absolute top-0 left-1 scale-75 transition-all  h-2 p-2 text-white  text-xs rounded-full ">
+      {cartItems.length}
     </span>}
     <input type="radio" id={e.page} className="navigationInput" name="nav"/>
     <label className="px-4"  for={e.page}>
