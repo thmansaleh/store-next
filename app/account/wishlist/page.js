bffaf28cb1491bd2e61a6e8df7be477c3c6c8588
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-25 03:30:03 +0400

    Update page.js

--- app/account/wishlist/page.js
+++ app/account/wishlist/page.js
@@ -1,6 +1,6 @@
 
 "use client"
-import Link from "next/Link"
+import Link from 'next/link'
 export default function page() {
    
 const items = [1,2,3,4,5]
@@ -10,7 +10,7 @@ return <div className="p-3 flex justify-around flex-wrap gap-y-6">
  {items.length>0?items.map((e,i)=>{
 
 
-return <Link key={i} href="/product?product_id=1" className=" inline-block relative rounded-lg   shadow-md overflow-hidden  w-36 bg-white   h-fit " >
+return <link key={i} href="/product?product_id=1" className=" inline-block relative rounded-lg   shadow-md overflow-hidden  w-36 bg-white   h-fit " >
     <div className="  "><img className="h-40 w-full object-contain " src="https://cdn.dummyjson.com/product-images/1/thumbnail.jpg" /></div><span className=" line-clamp-2 text-sm my-2 px-2">An apple mobile which is nothing like apple</span>
     <div className="p-2 text-sm bg-zinc-100  flex justify-between items-center"><span>
         549
@@ -18,7 +18,7 @@ return <Link key={i} href="/product?product_id=1" className=" inline-block relat
       </span>
       <svg className="fill-red-500 h-5 w-5" stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 16 16" height={20} width={20} xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z" /><path fillRule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clipRule="evenodd" /></svg>
     </div>
-  </Link>
+  </link>
 
 })
 :<h1>no content</h1>}
