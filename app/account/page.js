dc6d942f58658c1eda62e9c70ebdc9c129a2dec8
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-27 20:09:35 +0400

    Update page.js

--- app/account/page.js
+++ app/account/page.js
@@ -19,7 +19,9 @@ import LanguageModal from './LanguageModal'
     </div>
   </div>
   <div className="w-full h-28  bg-white rounded-2xl  flex items-center justify-around">
-    <Link href="./wishlist" className="w-24 inline-block rounded-lg text-center  py-1 hover:bg-neutral-200 ">
+
+
+    <Link href="./account/wishlist" className="w-24 inline-block rounded-lg text-center  py-1 hover:bg-neutral-200 ">
       <svg className="w-full h-7 fill-red-500" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width={512} height={512}><path d="M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917Z" /></svg>
       <span className="text-sm text-black font-semibold select-none">المفضلة</span>
       <span className="block text-gray-400 text-sm select-none">10</span>
