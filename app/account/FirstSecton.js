8c1c53fcae9d2870f20015d5a234a39186d28c57
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-28 21:42:56 +0400

    Update FirstSecton.js

--- app/account/FirstSecton.js
+++ app/account/FirstSecton.js
@@ -7,7 +7,8 @@ const wishlistCount = useSelector(state=>state.wishlist.items. length)
 const icons =[
 {
 name:'المفضلة',
-icon:<svg className="w-full h-7 fill-red-500" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width={512} height={512}><path d="M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917Z" /></svg>,
+icon:<svg className="w-full h-7 fill-red-500" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><circle cx="5.5" cy="15.5" r="1.5"/><path d="M19,3H5A5.006,5.006,0,0,0,0,8v8a5.006,5.006,0,0,0,5,5H19a5.006,5.006,0,0,0,5-5V8A5.006,5.006,0,0,0,19,3ZM5,5H19a3,3,0,0,1,3,3H2A3,3,0,0,1,5,5ZM19,19H5a3,3,0,0,1-3-3V10H22v6A3,3,0,0,1,19,19Z"/></svg>,
+
 count:wishlistCount,
 route:"./account/wishlist"
 },
