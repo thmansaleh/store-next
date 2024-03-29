c61caa0fb3400781ba36a0133c15512387cbf862
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-03-29 16:07:15 +0400

    Update EmptyCart.js

--- app/cart/EmptyCart.js
+++ app/cart/EmptyCart.js
@@ -5,7 +5,7 @@ export default function EmptyCart(){
 return <div className=" mt-24 flex items-center justify-center flex-col gap-7">
   <img className="w-36 h-36" src="https://cdni.iconscout.com/illustration/premium/thumb/empty-cart-7359557-6024626.png?f=webp" alt />
   <h2 className="text-md text-gray-500">السلة فارغة</h2>
-  <Link className="py-2 text-center w-36 inline-block font-semibold font bg-black text-white text-sm" href="./cart">تسوق</Link>
+  <Link className="py-2 text-center w-36 inline-block font-semibold font bg-black text-white text-sm" href="./">تسوق</Link>
 </div>
 
 }
