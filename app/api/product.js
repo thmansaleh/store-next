0059f67797a5403c7b7ee3610847762d79efa595
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-04-01 05:47:35 +0400

    Update app

---
+++ app/api/product.js
@@ -0,0 +1,22 @@
+async function product(url) {
+const fetcher =fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .catch(error => {
+    throw new Error('Error load');
+  });
+
+
+
+  const { data, error, isLoading } = useSWR('url', fetcher)
+ console.log(data)
+  return {
+    data,
+    isLoading,
+    isError: error 
+  }
+}
