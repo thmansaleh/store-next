6c12be34db0d7e108437606bbeb53aafdc69550a
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-04-01 17:42:14 +0400

    Update product.js

--- app/api/product.js
+++ app/api/product.js
@@ -5,12 +5,12 @@ export function useProduct() {
 const fetcher =fetch("https://mobapp.rcuae.ae/MOBAPP_V3_PROD/ERC_MOB_V2_WS.asmx/local_aids_file_info?p_user_id=139622&Lang=AR")
   .then(response => {
     if (!response.ok) {
-      
+      throw new Error("failed")
     }
     return response.json();
   })
   .catch(error => {
-    
+    throw new Error("false")
   });
 
 
