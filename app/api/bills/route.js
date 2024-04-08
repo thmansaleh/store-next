62a95bc815fe5e8e665b5fd98b119052039af411
othman <thman.saleh@gmail.com>
2024-04-09 03:56:42 +0400

    fdswq

--- app/api/bills/route.js
+++ app/api/bills/route.js
@@ -1,23 +1,24 @@
 
 import db from "../../config/db"
-export  async function GET({req}){
+export   function GET({req}){
+    return new Response('fkjdx')
 
-try {
-    const results = await new Promise((resolve,reject)=>{
-        db.query('SELECT * FROM bills WHERE status="لم يتم التسليم"',(err,result)=>{
-            if (err) {
-               reject(err) 
-            }else{
-                resolve(result)
-            }
-        })
-    })
+// try {
+//     const results =  new Promise((resolve,reject)=>{
+//         db.query('SELECT * FROM bills',(err,result)=>{
+//             if (err) {
+//                reject(err) 
+//             }else{
+//                 resolve(result)
+//             }
+//         })
+//     })
 
-    return  Response.json({results})    
-} catch (error) {
-    return  Response.json(error)
+//     return  Response.json({results})    
+// } catch (error) {
+//     return  Response.json(error)
 
-}
+// }
 
 
 
