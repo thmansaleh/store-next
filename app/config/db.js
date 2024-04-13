1e264aa3963ed974e770a605f8564991d2394693
othman <thman.saleh@gmail.com>
2024-04-13 20:08:55 +0400

    gf

--- app/config/db.js
+++ app/config/db.js
@@ -1,21 +1,19 @@
-// const mysql      = require('mysql');
-import mysql from 'mysql'
-const db = mysql.createConnection({
-  host     : '141.136.43.101',
-  user     : 'u581803121_ERC',
-  password : '100200300.aA',
-  database : 'u581803121_ERC'
-});
-db.connect(err=>{
-    if(err){
-        
-        console.log(err)
-        console.log('errror db not connect')
-    }else{
-        console.log(' db  connect')
 
-    }
-});
+import mysql2 from 'mysql2'
 
-// module.exports=db
+const db = mysql2.createConnection({
+    host     : '141.136.43.101',
+    user     : 'u581803121_ERC',
+    password : '100200300.aA',
+    database : 'u581803121_ERC'
+});
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+})
+
 export default db
+
