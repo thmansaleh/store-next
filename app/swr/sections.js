b2053aa16f90406e73e982c3c4ca2994a81a0a68
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-08 15:02:50 +0400

    Update app

---
+++ app/swr/sections.js
@@ -0,0 +1,20 @@
+'use client'
+
+import axios from 'axios';
+import fetchSections from "@/app/libs/services/fetchSections"
+import useSWR from 'swr'
+
+export function swrSections(){
+ const url ="https://dull-plum-antelope-tutu.cyclic.cloud/sections"
+  const fetcher = async () => {
+   const response = await fetchSections()
+ };
+const { data , error, isLoading } = useSWR(url, fetcher)
+return {
+  data,
+  isLoading,
+   error
+}
+
+
+}
