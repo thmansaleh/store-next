8d1a68ca4eb79a7fd943ab4d9c02b3e1a64980aa
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-08 15:04:44 +0400

    Update fetchSections.js

--- app/libs/services/fetchSections.js
+++ app/libs/services/fetchSections.js
@@ -1,1 +1,11 @@
-b
+
+import axios from 'axios';
+
+ const url ="https://dull-plum-antelope-tutu.cyclic.cloud/sections"
+ export const fetchSections= async () => {
+   const response = await axios.get(url);
+
+   return response.data;
+ };
+
+
