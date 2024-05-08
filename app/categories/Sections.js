0ef9950eb218d732096db556f6f1edec9841d8f4
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-08 15:11:02 +0400

    Update Sections.js

--- app/categories/Sections.js
+++ app/categories/Sections.js
@@ -1,31 +1,19 @@
+"use client"
 import styles from "./styles.css"
+import swrSections from "@/app/swr/sections"
 export default function Sections(){
-const categories = [
-  "رجال",
-  "نساء",
-  "أطفال",
-  "داخلية",
-  "رياضية",
-  "عربية",
-  "فساتين",
-  "جينز",
-  "قمصان",
-  "تنانير",
-  "شتاء",
-  "صيف",
-  "موحدة",
-  "إكسسوارات",
-  "مناسبات"
-];
+  const {data,error,isLoading} =swrSections()
+
 
   
 
-return <div>
-  {categories. map((e,i)=>{
+if{isLoading} return "loading"
+if{data} return <div>
+  {data. map((e,i)=>{
 return <div key={i+43}  className=" bg-gray-100  ">
-    <input className="hidden categoryInput" type="radio" id={e} name="category" />
-    <label className=" w-full p-2 block" htmlFor={e}>
-      {e}
+    <input className="hidden categoryInput" type="radio" id={e.id} name="category" />
+    <label className=" w-full p-2 block" htmlFor={e.id}>
+      {e.ar}
     </label>
   </div>
 
