76273defee9b800a5278827a7075430f4b599948
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-08 18:55:45 +0400

    Update Sections.js

--- app/categories/Sections.js
+++ app/categories/Sections.js
@@ -8,18 +8,7 @@ export default function Sections(){
   
 if(error) "error"
 if(isLoading) return "loading"
-if(data) return <div>
-  {data.map((e,i)=>{
-console.log(data)
-return <div key={i+43}  className=" bg-gray-100  ">
-    <input className="hidden categoryInput" type="radio" id={e.id} name="category" />
-    <label className=" w-full p-2 block" htmlFor={e.id}>
-      {e.ar}
-    </label>
-  </div>
-
-})}
-</div>
+if(data) console.log(data)
 
 
 }
