c2e0e49136575c075f5c866680eb9800c5ac466c
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-09 06:03:17 -0700

    Update sections.js

--- app/swr/sections.js
+++ app/swr/sections.js
@@ -1,29 +1,19 @@
 'use client'
 
 import axios from 'axios';
+
 import useSWR from 'swr'
 
-export function swrSections(){
-
-
-const url ="https://dull-plum-antelope-tutu.cyclic.cloud/sections"
-
- const fetchSections= async () => {
+export function swrSections (id){
+ const url ="https://dull-plum-antelope-tutu.cyclic.cloud/sections"
+  const fetchProduct = async () => {
    const response = await axios.get(url);
 
    return response.data;
  };
-
-
-  const fetcher = async () => {
-   const response = await fetchSections()
- };
-const { data , error, isLoading } = useSWR(url, fetcher)
+const { data , error, isLoading } = useSWR(url, fetchProduct,{ refreshInterval: 100000 })
 return {
   data,
   isLoading,
    error
-}
-
-
 }
