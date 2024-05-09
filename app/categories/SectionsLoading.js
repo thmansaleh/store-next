c4898f436625a7cd104d84274b6ccef0b102f7f5
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-09 06:25:57 -0700

    Update categories

---
+++ app/categories/SectionsLoading.js
@@ -0,0 +1,14 @@
+
+"use client"
+export default function SectionsLoading(){
+
+return <div className="animate-pulse  flex flex-col gap-y-2">
+
+  <div className="  h-8 w-full bg-gray-200"></div>
+{[1,1,1,1,1,1,1,1].map(e=>{
+ return  <div className="  h-8 w-full bg-gray-200"></div>
+
+})}
+ 
+</div>
+}
