35e95b9f7f4dbc9fe22d79d8fef57529e4e497ba
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-11 23:56:27 +0400

    Update Empty.js

--- app/account/wishlist/Empty.js
+++ app/account/wishlist/Empty.js
@@ -7,6 +7,6 @@ return <div className=" mt-6  flex flex-col items-center justify-center space-y-
     <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
   </svg>
   <h2 className="text-md font-bold">قائمة المفضلة فارغة</h2>
-  <Link href="./" className="p-2 px-6 rounded-lg bg-black text-white">تسوق </Link>
+  <Link href="/" className="p-2 px-8 rounded-lg bg-black text-white">تسوق </Link>
 </div>
 }
