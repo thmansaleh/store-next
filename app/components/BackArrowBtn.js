4b65f647e2d28adcb6873d821f477fbdda253397
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-12 03:26:42 +0400

    Update BackArrowBtn.js

--- app/components/BackArrowBtn.js
+++ app/components/BackArrowBtn.js
@@ -1,9 +1,18 @@
 "use client"
-
+import { useRouter } from 'next/router'
 export default function BackArrowBtn(){
 
+
+
+
+const router = useRouter()
+
+const back = (e) => {
+    
+    router.push("./")
+  }
 return <div className="sticky top-0 w-full h-10 bg-white">
-  <svg className="h-5 w-5 absolute top-1/2 start-4
+  <svg onClick={back}   className="h-5 w-5 absolute top-1/2 start-4
 ltr:rotate-180 transform   -translate-y-1/2" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xmlSpace="preserve">
     <path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
 	c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
