d230947575eac1bb55a5429c74d1f63440e45898
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-12 13:42:29 +0400

    Update [id]

---
+++ app/brands/[id]/Information.js
@@ -0,0 +1,12 @@
+export default function Information(){
+
+
+return 
+<div className="text-center space-y-4 mt-4">
+  <h1 class="text-xl font-medium text-black  ">Next </h1>
+  <p className="text-base">
+    lorem site is a Partner of our esteemed company Quarter Decorative Engineer and a great opportunity to join your team and contribute to 
+  </p>
+</div>
+
+}
