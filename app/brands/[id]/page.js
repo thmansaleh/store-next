821346b996a0d7579ca9f40b4fb7e1c72d038945
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-12 13:41:57 +0400

    Update page.js

--- app/brands/[id]/page.js
+++ app/brands/[id]/page.js
@@ -1,14 +1,11 @@
 
-
+import Information  from "./Information"
+import BackArrowBtn from "@/app/components/BackArrowBtn"
 export default function(){
+return <>
+<BackArrowBtn/>
+<Information/>
 
-
-return 
-<div className="text-center space-y-4 mt-4">
-  <h1 class="text-xl font-medium text-black  ">Next </h1>
-  <p className="text-base">
-    lorem site is a Partner of our esteemed company Quarter Decorative Engineer and a great opportunity to join your team and contribute to 
-  </p>
-</div>
+</>
 
 }
