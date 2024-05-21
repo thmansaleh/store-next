f6c457d29e712c1f385f07d779428878dc1d40ac
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-21 17:40:06 +0400

    Update FullScreen.js

--- app/FullScreen.js
+++ app/FullScreen.js
@@ -1,25 +1,25 @@
 "use client"
 
-import { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
-const FullScreen = () => {
-  const fullscreenRef = useRef(null);
-
-  const handleFullscreen = () => {
-    const element = fullscreenRef.current || document.documentElement;
-
-    if (element.requestFullscreen) {
-      element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) { // Firefox
-      element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) { // Chrome, Safari, and Opera
-      element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) { // IE/Edge
-      element.msRequestFullscreen();
-    }
-  };
-
+const FullscreenComponent = () => {
   useEffect(() => {
+    const handleFullscreen = () => {
+      const element = document.documentElement;
+
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.mozRequestFullScreen) { // Firefox
+        element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+        element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) { // IE/Edge
+        element.msRequestFullscreen();
+      }
+    };
+
+    const fullscreenTimeout = setTimeout(handleFullscreen, 1000); // Delay of 1 second
+
     const handleFullscreenChange = () => {
       if (!document.fullscreenElement) {
         console.log('Exited fullscreen mode');
@@ -29,17 +29,18 @@ const FullScreen = () => {
     };
 
     document.addEventListener('fullscreenchange', handleFullscreenChange);
+
     return () => {
+      clearTimeout(fullscreenTimeout);
       document.removeEventListener('fullscreenchange', handleFullscreenChange);
     };
   }, []);
 
   return (
-    <div ref={fullscreenRef}>
-      <button onClick={handleFullscreen}>Go Fullscreen</button>
+    <div>
       {/* Your content goes here */}
     </div>
   );
 };
 
-export default FullScreen;
+export default FullscreenComponent;
