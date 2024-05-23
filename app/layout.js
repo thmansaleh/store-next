5cc5bd0858924ccf442a795a3e0d4b7df8030f72
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-23 15:10:19 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,9 +7,13 @@ export const metadata = {
   description: 'app',
 }
 
+import Head from 'next/head';
 export default function RootLayout({ children }) {
   return (
     <html lang="en" dir="rtl">
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    </Head>
       <body className="font-sans pb-16">
 <FullScreen/>
         <ProviderPage children={children}/>
