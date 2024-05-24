6605ab8522342e2f65b33c91d5b953d84bffa98e
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-24 06:51:45 +0400

    Update ProductInformation.js

--- app/product/[id]/ProductInformation.js
+++ app/product/[id]/ProductInformation.js
@@ -25,7 +25,7 @@ export default function ProductInformation({id}){
   <div className="flex items-baseline mt-2">
     <div className="text-xl font-bold text-red-600">{data.price} د.إ</div>
     <div className="text-sm text-gray-500 line-through ml-2">{data.price} د.إ</div>
-    <div className="text-sm font-semibold text-red-500 ml-2">تخفيض -60% عند الشراء </div>
+    <div className="text-sm font-semibold text-red-500 ml-2">تخفيض  {data.discountPercentage}%</div>
   </div>
   <div className="text-sm text-gray-500 mt-1">توصيل في  <span className="text-green-400">16 نوفمبر, الثلاثاء</span></div>
 
