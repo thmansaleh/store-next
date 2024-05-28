1a050d6b6fe5c1294348f67a8eb60fc27d9739e8
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-29 00:44:30 +0400

    Update home

---
+++ app/home/free-return-section/FreeReturnSection.js
@@ -0,0 +1,39 @@
+"use client"
+
+
+export default function FreeReturnSection(){
+
+return 
+<div class="bg-white my-3 p-2">
+
+    <div class="bg-white text-sm p-4 rounded-lg shadow-md flex justify-between items-center w-full max-w-lg">
+    
+        <div class="flex items-center gap-x-2">
+            <svg class="h-6 w-6 fill-gray-600" >
+            <path d="M12,2a10.032,10.032,0,0,1,7.122,3H16a1,1,0,0,0-1,1h0a1,1,0,0,0,1,1h4.143A1.858,1.858,0,0,0,22,5.143V1a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1V3.078A11.981,11.981,0,0,0,.05,10.9a1.007,1.007,0,0,0,1,1.1h0a.982.982,0,0,0,.989-.878A10.014,10.014,0,0,1,12,2Z"/><path d="M22.951,12a.982.982,0,0,0-.989.878A9.986,9.986,0,0,1,4.878,19H8a1,1,0,0,0,1-1H9a1,1,0,0,0-1-1H3.857A1.856,1.856,0,0,0,2,18.857V23a1,1,0,0,0,1,1H3a1,1,0,0,0,1-1V20.922A11.981,11.981,0,0,0,23.95,13.1a1.007,1.007,0,0,0-1-1.1Z"/></svg>
+
+            <div>
+                <p class="font-semibold">إرجاع مجاني</p>
+                <p class="text-sm text-gray-600">لمدة 90 يوم</p>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    </div>
+    
+</div>
+}
