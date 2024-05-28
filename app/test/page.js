5c9ccd1cde04eb62ef090f9b4f5b223984d72706
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-28 06:39:09 +0400

    Update page.js

--- app/test/page.js
+++ app/test/page.js
@@ -4,7 +4,7 @@ import MyImageComponent from "./MyImageComponent"
 import { swrSingleProduct } from '@/app/libs/swr/singleProduct';
 export default function(){
  
-  const {data,error,isLoading} =swrSingleProduct(40)
+  const {data,error,isLoading} =swrSingleProduct(43)
   if (error) return <div>failed to load</div>
 if(data) return data.images.map(e=>{
 return  <MyImageComponent src={e} />})
