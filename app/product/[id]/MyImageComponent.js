1dc4d59965ed46a07176060fbd8fd0c375d984eb
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-05-28 06:44:19 +0400

    Update [id]

---
+++ app/product/[id]/MyImageComponent.js
@@ -0,0 +1,27 @@
+"use client"
+
+import { useState } from 'react';
+import Image from 'next/image'
+
+const SwiperImageComponent = ({ src }) => {
+  const [isLoading, setIsLoading] = useState(true); // State to track loading
+
+  return (
+    <div className="relative w-40 h-40">
+      {isLoading && (
+        <div style={{ position: 'absolute', inset: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <span>Loading...</span> {/* You can replace this with a spinner component */}
+        </div>
+      )}
+      <Image
+        src={src}
+        alt="jjhfff"
+        fill={true}
+        style={{ objectFit: 'cover' }} // Using style prop for object-fit
+        onLoadingComplete={() => setIsLoading(false)}
+      />
+    </div>
+  );
+};
+
+export default SwiperImageComponent
