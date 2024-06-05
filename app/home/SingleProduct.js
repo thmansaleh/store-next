1133321a6ab75e88de22e1dab25c0cce31901d77
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-06-06 00:52:11 +0400

    Update SingleProduct.js

--- app/home/SingleProduct.js
+++ app/home/SingleProduct.js
@@ -12,7 +12,7 @@ prefetch={true} href={{
 
   }} className=" inline-block relative rounded-lg  my-2 shadow-md overflow-hidden  w-44 bg-white   h-fit ">
     <span className=" bg-red-700 text-white top-0  text-center z-20 absolute rounded-br-lg  rtl:rounded-bl-lg rtl:rounded-br-none  px-3 text-xs bg-blend-multiply line-height start-0 opacity-80 ">خصم 20%</span>
-    <div className=" w-full h-48 overflow-hidden relative ">
+    <div className=" w-full   relative ">
 
 
 
