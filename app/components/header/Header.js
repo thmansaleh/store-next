3ca3b883011641fc958e76f5d6499e2e713a59f6
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-06-19 15:35:16 +0400

    Update Header.js

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -3,7 +3,7 @@
 export default function Header(){
 
 
-return <div className="rounded-lg p-2 sticky top-0 bg-white flex items-center justify-between">
+return <div className="rounded-lg p-2 sticky top-0 bg-white flex items-center z-50 justify-between">
   <div>
     <svg className="w-10 h-10 p-2  fill-back-500 bg-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={512} height={512}><g id="_01_align_center" data-name="01 align center"><path d="M24,22.586l-6.262-6.262a10.016,10.016,0,1,0-1.414,1.414L22.586,24ZM10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18Z" /></g></svg>
   </div>
