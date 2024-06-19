d75425a85e0b0c640ca0bd7ac5a1517ad339ac15
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-06-19 15:20:56 +0400

    Update Home.js

--- app/home/Home.js
+++ app/home/Home.js
@@ -1,6 +1,6 @@
 "use client"
 import SetNave from '@/app//components/SetNave'
-import SearchInput from "@/app/components/SearchInput"
+import Header from "@/app/components/header/Header"
 import FristSlider from './FristSlider'
 import Products from'@/app/home/Products'
 import SwiperFreemodeSection from './SwiperFreemodeSection'
@@ -10,7 +10,7 @@ import FreeReturnSection from "./free-return-section/FreeReturnSection"
 export default function Home({products}){
   return <>
     {/* <SetNave name='/' display={true}/> */}
-<SearchInput/>
+<Header/>
 <FristSlider/>
 
 
