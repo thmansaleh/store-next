3b0c26877fb10ab6032f74aa56e7db9e15d31ae9
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-06-20 14:45:51 +0400

    Update NavigationBar.js

--- app/components/navigationBar/NavigationBar.js
+++ app/components/navigationBar/NavigationBar.js
@@ -2,7 +2,6 @@
 import styles from './styles.css'
  import { useSelector } from 'react-redux';
 import {useRef } from "react"
-import Link from 'next/link'
  import { useRouter } from 'next/navigation'
 import {icons} from "./NavigationIcons"
 export default function NavigationBar(){
@@ -22,8 +21,9 @@ router.push('/cart')
 return  <div ref={navigationRef}  className=" z-50 fixed border-solid border-t border-inherit  fixegd rounded-t-lg bottom-0  w-screen py-4 bg-white  flex items-center  justify-around ">
   
   {icons.map((e,i)=>{
-const url = `/${e.page}`
- return  <Link href={url} dir='rtl' key={i} 
+ return  <Link  dir='rtl' key={i} onClick={()=>{
+router.push(`/${e.page}`,{ scroll: false })
+}}
 
 className="flex items-center justify-center bg-grvay-500  h-10   overflow-hidden   relative">
 {e.page=="cart"? alert&&<span class="bg-red-500 flex items-center justify-center w-2 absolute top-0 left-1 scale-75 transition-all  h-2 p-2 text-white  text-xs rounded-full ">
@@ -36,7 +36,7 @@ className="flex items-center justify-center bg-grvay-500  h-10   overflow-hidden
       <span className="bg-black  -bottom-2 inset-x-1/2    translate-x-1/2 absolute  h-1.5 w-1.5  rounded-full  transition-all duration-300"></span>
     </label>
   
-  </Link>
+  </div>
 
 
 })}
