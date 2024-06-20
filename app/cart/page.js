9eba5e098d2fbf609def85867c4c30b060e1e630
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-06-20 18:52:50 +0400

    Update page.js

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,6 +1,7 @@
 
 "use client"
 import {useEffect} from "react"
+import BackArrowBtn from "@/app/components/BackArrowBtn"
 import SetNave from '../components/SetNave'
 import OrderDetailes from './OrderDetailes'
 import PromotionCode from "./PromotionCode"
@@ -12,14 +13,18 @@ import  EmptyCart from "./EmptyCart"
 export default function Cart(){
     const dispatch = useDispatch()
     const cartItems = useSelector(state => state.cart.items);
-console.log(cartItems)
+
 
 useEffect(()=>{
 
 dispatch(changeAlertStatus())
 },[])
+
+
 if(cartItems.length>0){
-  return <div style={{"background-color":"#f7f7f7"}} className=" p-3">
+  return <>
+<BackArrowBtn/>
+ <div style={{"background-color":"#f7f7f7"}} className=" p-3">
   <div className="my-2">
 
 <SetNave name='cart' display={true}/>
@@ -28,8 +33,12 @@ if(cartItems.length>0){
 <OrderDetailes items={cartItems}/>
 </div>
 </div>
+</>
 }else{
-return <EmptyCart/>
+return <>
+<BackArrowBtn/>
+<EmptyCart/>
+</>
 }
 }
 
