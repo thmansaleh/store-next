ff93e36d1f49b929dda140be5a857cd1963536fb
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-06-29 15:54:06 +0400

    Update PaymentMethods.js

--- app/checkout/PaymentMethods.js
+++ app/checkout/PaymentMethods.js
@@ -30,11 +30,11 @@ export default function(){
   ]
   if( true) return <div className="w-full p-2 border border-gray-300 rounded-lg flex items-center  justify-center gap-x-6 ">
     {data.map(payment=>{
-      return  <div  key={payment.value} onClick={()=>dispatch(changePaymentMethod(payment.value))} className=" inline-block text-center  ">
+      return  <div  key={payment.value} onClick={()=>dispatch(changePaymentMethod(payment.value))} className=" transition-all inline-block text-center  ">
       <input id={payment.value} name="payment" className="hidden payments" type="radio" />
       <label htmlFor={payment.value}>
         <div className="w-20 h-20 overflow-hidden flex flex-wrap p-2  items-center justify-center bg-white rounded-lg ">
-        <img className="grayscale w-full h-12   object-contain" src={payment.img} alt={payment.value}/>
+        <img className=" transition-all grayscale w-full h-12   object-contain" src={payment.img} alt={payment.value}/>
           <span className="text-sm   inline-block ">{payment.name}</span>
         </div>
       </label>
