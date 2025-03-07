1d89da9dd7d6bfeb3348ea2b8a3b878efa21afaa
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-03-07 20:37:03 +0400

    Update Card.js

--- app/checkout/Card.js
+++ app/checkout/Card.js
@@ -6,28 +6,27 @@ export default function Card(){
   const paymentMethod=useSelector(state=>state.checkout.paymentMethod)
   
 if(paymentMethod=='card') return <>
-  <h3 className="text-md font-semibold my-5">الدفع عبر بطاقتك البنكية</h3>
-
-<div className="mx-auto max-w-lg mt-10 ring-2 rounded-lg ring-gray-500 ">
-
-  <div className="bg-white rounded-lg overflow-hidden shadow-lg">
-    <div className="px-6 py-4">
-      <div className="flex justify-between items-center">
-        <img className="h-8" src="https://www.svgrepo.com/show/499847/company.svg" alt="Workflow logo" />
-        <input placeholder="تاريخ الانتهاء" type="text" className="font-medium outline-none w-1/4 text-gray-600" />
+  
+      
+      <div className="mb-8">
+  <h2 className="text-xl font-semibold text-gray-700 mb-4">معلومات الدفع</h2>
+  <form className="space-y-4">
+    <div>
+      <label className="block text-sm font-medium text-gray-700">رقم البطاقة</label>
+      <input type="text" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500" placeholder="123 123 123 123" />
+    </div>
+    <div className="grid grid-cols-2 gap-4">
+      <div>
+        <label className="block text-sm font-medium text-gray-700">تاريخ الانتهاء</label>
+        <input type="text" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500" placeholder="MM/YY" />
       </div>
-      <div className="mt-4">
-        <input type="text" className="font-bold outline-none text-gray-800 text-md" placeholder="رقم البطاقة" />
-        <div className="flex justify-between items-center mt-2">
-          <input type="text" className="text-sm outline-none font-semibold text-gray-600" placeholder="اسم حامل البطاقة" />
-          <img className="h-10 w-10" src="https://www.svgrepo.com/show/362011/mastercard.svg" alt="Mastercard logo" />
-        </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">CVV</label>
+        <input type="text" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500" placeholder={123} />
       </div>
     </div>
-    <div className="bg-gray-100 px-6 py-4">
-      <input type="number" className=" outline-none text-md w-fit bg-transparent font-bold text-gray-800 mt-2" placeholder="CVC" />
-    </div>
-  </div>
+  </form>
 </div>
+    
 </>
 }
