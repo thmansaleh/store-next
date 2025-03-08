5dd3c58ab7e9e68bc1e616023cb490649b13f199
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-03-08 18:06:22 +0400

    Create Categories.js

---
+++ app/categories/Categories.js
@@ -0,0 +1,13 @@
+export default function Categories(){
+
+return <div className="p-5">
+  <div className="flex gap-x-6 gap-y-8 font-bold text-base  flex-wrap">
+    <div>
+      <div className="bg-gray-50 shadow-sm h-20 w-20 rounded-full">
+        <img className="w-full h-full object-contain" src="https://store-next-wheat.vercel.app/_next/image?url=https%3A%2F%2Fcdn.dummyjson.com%2Fproducts%2Fimages%2Fbeauty%2FRed%2520Lipstick%2F1.png&w=1200&q=75" alt="https://store-next-wheat.vercel.app/_next/image?url=https%3A%2F%2Fcdn.dummyjson.com%2Fproducts%2Fimages%2Fbeauty%2FRed%2520Lipstick%2F1.png&w=1200&q=75" />
+      </div>
+      <div className="text-center mt-2">خواتم</div>
+    </div>
+  </div>
+</div>
+}
