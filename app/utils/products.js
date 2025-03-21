505d730923fa033da9bec1c5a6d0c81ca786429e
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-03-21 20:53:31 +0400

    Update products.js

--- app/utils/products.js
+++ app/utils/products.js
@@ -38,6 +38,29 @@ export async function getProductsByCategory(categoryId, options = {}) {
     }
 }
 
+
+
+export async function getProductImages(product_id) {
+    try {
+        const response = await fetch(`${host}/api/images/${product_id}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching product images:', error);
+        throw error;
+    }
+}
+
+
+
+
 export async function getProductsBySeller(sellerId, options = {}) {
     try {
         const response = await fetch(`${host}/api/products/seller/${sellerId}`, {
