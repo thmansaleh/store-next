0393077436a6bfc55b7b19e3aa2a837f27beb0d4
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-04-09 17:09:55 +0400

    Update ProductInformation.js

--- app/product/[id]/ProductInformation.js
+++ app/product/[id]/ProductInformation.js
@@ -1,19 +1,18 @@
 
 'use client'
 import { useProduct } from '@/app/api/product';
-import { swrSingleProduct } from '@/app/libs/swr/singleProduct';
 
-export default function ProductInformation({id}){
+export default function ProductInformation({product}){
 
-function calculateOriginalPrice(finalPrice, discountPercentage) {
-   let number=finalPrice / (1 - discountPercentage / 100)
+function calculateOriginalPrice(price) {
+   let number=price / (1 - price / 100)
  return Math.round(number * 10) / 10;
 
 
 }
 
 
-  const {data,error,isLoading} =swrSingleProduct(id)
+ 
 
 
   
@@ -32,7 +31,7 @@ function calculateOriginalPrice(finalPrice, discountPercentage) {
 }
 
 
-  if(isLoading) return <div className="  w-full ">
+ 
 <div className="animate-pulse my-4 ">
 
   <div className="w-full space-y-3 ">
@@ -46,12 +45,12 @@ function calculateOriginalPrice(finalPrice, discountPercentage) {
 </div>
 
   return <div className="bg-white py-4 ">
-  <div className="text-lg font-bold text-gray-800">{data.brand}</div>
-  <div className="text-sm text-gray-500">{data.title}</div>
+  <div className="text-lg font-bold text-gray-800">{product.name}</div>
+  <div className="text-sm text-gray-500">{product.name}</div>
   <div className="flex items-baseline mt-2">
-    <div className="text-xl font-bold text-red-600">{data.price} د.إ</div>
-    <div className="text-sm text-gray-500 line-through ml-2">{calculateOriginalPrice(data.price,data.discountPercentage)} د.إ</div>
-    <div className="text-sm font-semibold text-red-500 ml-2">تخفيض  {data.discountPercentage}%</div>
+    <div className="text-xl font-bold text-red-600">{product.price} د.إ</div>
+    <div className="text-sm text-gray-500 line-through ml-2">{calculateOriginalPrice(product.price)} د.إ</div>
+    <div className="text-sm font-semibold text-red-500 ml-2">تخفيض  {product.price}%</div>
   </div>
   <div className="text-sm text-gray-500 mt-1">توصيل في  <span className="text-green-400">{getArabicDate()}</span></div>
 
