abd430da6cf52c1270c35f862942a9170c359bc5
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-04-09 17:10:22 +0400

    Update Slider.js

--- app/product/[id]/Slider.js
+++ app/product/[id]/Slider.js
@@ -49,7 +49,7 @@ slidesPerView={1}
 
 {images.map((href,i)=>{
 
-if(href.image_url.length>3) <SwiperSlide key={href.image_id} 
+if(href.image_url.length<3) <SwiperSlide key={href.image_id} 
 
 
 className="mb-8"  >
