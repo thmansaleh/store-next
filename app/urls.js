2e74f2ea6a9e883d49a054605ab8818494305bde
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-04-09 14:08:42 +0400

    Update urls.js

--- app/urls.js
+++ app/urls.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL='https://store-api-new.vercel.app/api'
-export const API_BASE_URL='http://localhost:4000/api'
+ export const API_BASE_URL='https://store-api-new.vercel.app/api'
+// export const API_BASE_URL='http://localhost:4000/api'
