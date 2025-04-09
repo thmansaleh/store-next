b6f21349a4ec44d5f209bac1920ce39ed175526d
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-04-09 17:49:42 +0400

    Update Description.js

--- app/product/[id]/Description.js
+++ app/product/[id]/Description.js
@@ -1,13 +1,12 @@
+const Description = ({ description }) => {
+  if (!description) return null;
 
-const Description = ({description}) => {
- 
-if(description) return <div className="w-1/2">
-  <h2 className="text-md font-bold mb-4">الوصف</h2>
-  <p className="text-gray-700 mb-4 text-sm ">{description}</p>
+  return (
+    <div className="w-1/2">
+      <h2 className="text-md font-bold mb-4">الوصف</h2>
+      <p className="text-gray-700 mb-4 text-sm">{description}</p>
+    </div>
+  );
+};
 
-</div>
-
-  
-}
-
-export default Description
+export default Description;
