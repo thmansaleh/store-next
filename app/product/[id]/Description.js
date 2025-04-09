bc382084fb7f4d8133994c09d4eddafa89391edb
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-04-09 17:25:35 +0400

    Update Description.js

--- app/product/[id]/Description.js
+++ app/product/[id]/Description.js
@@ -1,12 +1,9 @@
 
-
-'use client'
-import { swrSingleProduct } from '@/app/libs/swr/singleProduct';
-const Description = ({id}) => {
-  const {data,error,isLoading} =swrSingleProduct(id)
-if(data) return <div className="w-1/2">
+const Description = ({description}) => {
+ 
+if(description) return <div className="w-1/2">
   <h2 className="text-md font-bold mb-4">الوصف</h2>
-  <p className="text-gray-700 mb-4 text-sm ">{data.description}</p>
+  <p className="text-gray-700 mb-4 text-sm ">{description}</p>
 
 </div>
 
