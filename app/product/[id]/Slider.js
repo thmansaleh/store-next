456885f3cd26628e09442569e36035c376befa0d
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-04-11 20:46:52 +0400

    Update Slider.js

--- app/product/[id]/Slider.js
+++ app/product/[id]/Slider.js
@@ -1,7 +1,8 @@
 "use client"
+import { useState } from 'react';
 import ImageComponent from "../../components/ImageComponent"
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y, Thumbs } from 'swiper';
 
 import './slider.css'
 import 'swiper/css';
@@ -9,77 +10,75 @@ import 'swiper/css/autoplay'
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import 'swiper/css/thumbs';
 import ShareHeartIcons from "./share-heart/ShareHeartIcons"
 import SliderLoading from "./SliderLoading"
 import { useProduct } from '@/app/api/product';
 import { swrSingleProduct } from '@/app/libs/swr/singleProduct';
 
 export default function Slider({images}) {
-  // console.log(images[0])
-  // return null
-return <>
-
-
-<Swiper 
-// loop={images.length}
-style={{
-  "--swiper-pagination-color": "black ",
-  "--swiper-pagination-bullet-inactive-color": "#999999",
-  "--swiper-pagination-bullet-inactive-opacity": "1",
-  "--swiper-pagination-bullet-size": "10px",
-  "--swiper-pagination-bullet-horizontal-gap": "6px"
-}}
-
-className=' relative  product-swiper-container' modules={[Pagination]}
-spaceBetween={50}
-rewind={true}
-slidesPerView={1}
- pagination={{
- clickable: true ,
- dynamicBullets: true,
-
-
-
-}}
-
->
-
-
-{/* <ShareHeartIcons/> */}
-
-{images.map((href,i)=>{
-
-if(href.image_url.length>3) 
-return <SwiperSlide key={href.image_id} 
-
-
-className="mb-8"  >
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
   
-   
-
-<ImageComponent src={href.image_url}       height='50vh'
-/>
-
-
-
-
-
-  </SwiperSlide>
-
-
-})}
-
-
-
-
-
-
-
-
-
-
-</Swiper>
-
-</>
-
-};
+  return (
+    <div className="slider-container">
+      <Swiper 
+        style={{
+          "--swiper-pagination-color": "black",
+          "--swiper-pagination-bullet-inactive-color": "#999999",
+          "--swiper-pagination-bullet-inactive-opacity": "1",
+          "--swiper-pagination-bullet-size": "10px",
+          "--swiper-pagination-bullet-horizontal-gap": "6px"
+        }}
+        className='relative product-swiper-container' 
+        modules={[Pagination, Thumbs]}
+        spaceBetween={50}
+        rewind={true}
+        slidesPerView={1}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        thumbs={{ swiper: thumbsSwiper }}
+      >
+        {images.map((href, i) => {
+          if(href.image_url.length > 3) 
+            return (
+              <SwiperSlide key={href.image_id} className="mb-8">
+                <ImageComponent src={href.image_url} height='50vh' />
+              </SwiperSlide>
+            );
+        })}
+      </Swiper>
+      
+      {/* Thumbnail Swiper */}
+      <div className="thumbnail-container mt-4">
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          spaceBetween={10}
+          slidesPerView="auto"
+          watchSlidesProgress={true}
+          modules={[Thumbs]}
+          className="thumbnail-swiper"
+        >
+          {images.map((href, i) => {
+            if(href.image_url.length > 3) 
+              return (
+                <SwiperSlide key={`thumb-${href.image_id}`} className="thumbnail-slide">
+                  <div className="soldier-thumbnail">
+                    <ImageComponent 
+                      src={href.image_url} 
+                      height='60px'
+                      className="thumbnail-image"
+                    />
+                    <div className="soldier-overlay">
+                      <div className="soldier-icon"></div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              );
+          })}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
