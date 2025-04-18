63d1c9aef7f5ffc9393d936af1ff9a6e528d7589
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-04-18 17:32:55 +0400

    Update email.js

--- app/utils/email.js
+++ app/utils/email.js
@@ -1,16 +1,14 @@
-const BASE_URL = 'http://localhost:3000/api/email';  // Replace with your actual base URL
-
-
+import { API_BASE_URL } from "../urls";
 // Function to send OTP verification email
 export async function sendOTPVerification({ name, email }) {
-    const response = await fetch(`${BASE_URL}/send-otp-verification?name=${name}&email=${email}`);
+    const response = await fetch(`${API_BASE_URL}/send-otp-verification?name=${name}&email=${email}`);
     const data = await response.json();
     return data;
 }
 
 // Function to verify the OTP
 export async function verifyOTP(otp, email) {
-    const response = await fetch(`${BASE_URL}/verify-otp?otp=${otp}&email=${email}`);
+    const response = await fetch(`${API_BASE_URL}/verify-otp?otp=${otp}&email=${email}`);
     const data = await response.json();
     return data;
 }
