d81ce1bd6d8921a3131d91482c33db5b8219dd84
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-04-28 10:34:21 +0400

    Update pay.js

--- app/utils/pay.js
+++ app/utils/pay.js
@@ -1,14 +1,21 @@
 import axios from 'axios';
 import { API_BASE_URL } from "../urls";
-export const processPayment = async (paymentData,addressId,userToken) => {
+
+export const processPayment = async (paymentData, addressId, userToken) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/pay`, {
-      addressId: addressId, // Pass data as query parameters
-      headers: {
-        'userToken': userToken,
-        'Content-Type': 'application/json',
+    const response = await axios.post(
+      `${API_BASE_URL}/pay`,
+      { 
+        addressId: addressId, 
+        paymentData: paymentData // You probably want to send paymentData too
+      },
+      {
+        headers: {
+          'userToken': userToken,
+          'Content-Type': 'application/json',
+        }
       }
-    });
+    );
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.error || 'Error processing payment');
