3d2e3aff875bb0570a2fa8015338d3e3a123bfb3
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-04-28 10:27:41 +0400

    Update pay.js

--- app/utils/pay.js
+++ app/utils/pay.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_BASE_URL } from "../urls";
 export const processPayment = async (paymentData,addressId,userToken) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/pay`, {
+    const response = await axios.post(`${API_BASE_URL}/pay`, {
       addressId: addressId, // Pass data as query parameters
       headers: {
         'userToken': userToken,
