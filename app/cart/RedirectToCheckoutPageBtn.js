0ad752b8f2d796c8778c5f8975d70b2d32497e07
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-04-28 09:01:56 +0400

    Update RedirectToCheckoutPageBtn.js

--- app/cart/RedirectToCheckoutPageBtn.js
+++ app/cart/RedirectToCheckoutPageBtn.js
@@ -1,14 +1,20 @@
 'use client'
 
 import { useDispatch } from "react-redux"
+import { redirect } from 'next/navigation'
 import { openModal } from "../store/features/AuthModal";
 import AuthModal from "../account/login/AuthModal";
 
 function RedirectToCheckoutPageBtn() {
     const dispatch=useDispatch()
     const handleOpenModal = () => {
-        dispatch(openModal());
+        const userToken= localStorage.getItem ('userToken')
       };
+if(userToken){
+redirect("./checkout")
+}else{
+dispatch(openModal());
+}
   return <>
   <AuthModal/>
     <button onClick={handleOpenModal} className="md:w-24 w-full bg-black text-white text-md font-semibold rounded-lg py-2 my-2">
