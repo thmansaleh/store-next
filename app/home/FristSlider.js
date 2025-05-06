ff3f08921dae8bcf49e846b2866ac9b6cd47d8d0
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-05-07 03:51:12 +0400

    Update FristSlider.js

--- app/home/FristSlider.js
+++ app/home/FristSlider.js
@@ -1,32 +1,11 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/autoplay';
 
 export default function CleanSlider() {
-  const [screenHeight, setScreenHeight] = useState(0);
-
-  // Calculate screen height on client side
-  useEffect(() => {
-    const updateScreenHeight = () => {
-      setScreenHeight(window.innerHeight);
-    };
-    
-    // Set initial value
-    updateScreenHeight();
-    
-    // Update on resize
-    window.addEventListener('resize', updateScreenHeight);
-    
-    return () => {
-      window.removeEventListener('resize', updateScreenHeight);
-    };
-  }, []);
-
-  const sliderHeight = screenHeight / 3; // One third of device height
-
   const images = [
     {
       img: 'https://firebasestorage.googleapis.com/v0/b/store-6d415.firebasestorage.app/o/products%2F1743789819976%2F1000200857.jpg?alt=media&token=50ffefde-24dc-4ecd-946a-a694e9d42b65',
@@ -38,6 +17,9 @@ export default function CleanSlider() {
       img: 'https://firebasestorage.googleapis.com/v0/b/store-6d415.firebasestorage.app/o/products%2F1743790205450%2F1000200861.jpg?alt=media&token=2292f196-8113-4ba3-a71b-ba57ba175525',
     },
   ];
+
+  // ثابت للارتفاع - تقريباً ثلث ارتفاع شاشة iPhone Pro Max
+  const fixedHeight = 930;
 
   return (
     <div className="max-w-6xl mx-auto">
@@ -53,7 +35,7 @@ export default function CleanSlider() {
         }}
         autoplay={{ delay: 4000, disableOnInteraction: false }}
         className="py-4"
-        style={{ height: screenHeight > 0 ? `${sliderHeight}px` : '33vh' }}
+        style={{ height: `${fixedHeight}px` }}
       >
         {images.map((item, i) => (
           <SwiperSlide key={i} style={{ height: '100%' }}>
