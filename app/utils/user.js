dd229a92663d62dbe96a2e70742aa38d82197c7f
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-05-07 00:13:15 +0400

    Update user.js

--- app/utils/user.js
+++ app/utils/user.js
@@ -1,9 +1,9 @@
-
+import { API_BASE_URL } from "../urls";
 export async function getUsername(token) {
   try {
     if (!token) return null;
     
-    const response = await fetch(`/api/get-username?token=${encodeURIComponent(token)}`);
+    const response = await fetch(`${API_BASE_URL}/get-username?token=${token}`);
     const data = await response.json();
     
     return data.success ? { name: data.name, userId: data.userId } : null;
