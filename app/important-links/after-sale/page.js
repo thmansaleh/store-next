94f8fcc2e4bd81ad86cab4020886fd682793c9b2
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-05-07 04:03:51 +0400

    Update page.js

--- app/important-links/after-sale/page.js
+++ app/important-links/after-sale/page.js
@@ -1,9 +1,32 @@
-
 function page() {
   return (
-<div className="content-entry p-3">
-  <p><strong>خدمات مجانيه ما بعد البيع.</strong></p><p><br /></p><p className="ql-align-justify">هدفنا رضاكم وقمنا بتوفير الخدمات التأليه.</p><ol><li className="ql-align-justify">في حالة وصول مقاس المنتج كبير او صغير نقوم بتعديل المقاس.</li><li className="ql-align-justify">في حالة تغير طلاء المنتج الاكسسوارات يتم صيانة المنتج.</li><li className="ql-align-justify">في حالة وصول المنتج ويوجد فيه اخطأ مصنعيه يتم تعديله.</li><li className="ql-align-justify">في حالة تأخر الطلب عن موعده المحدد بإمكانك طلب الإسراع من خلال<a href="https://docs.google.com/forms/d/e/1FAIpQLSf9CEmoKl6WTCNCouMA1VRmoG0stFnye0t79PPLRVORHU_Kjw/viewform" target="_blank"> <strong>الضغط هنا</strong></a></li></ol><p className="ql-align-justify"><br /></p><p className="ql-align-right"><strong>ملاحضات هامة</strong></p><p className="ql-align-right"><strong>﻿1. في حالة طلب إحداء الخدمات المجانية تكاليف الشحن و التوصيل يدفعها العميل وتتكلف رافال ف صيانة المنتج مجان.</strong></p><p className="ql-align-right"><strong>﻿2. بامكنك طلب إحداء الخدمات المجانية بالتواصل واتساب</strong></p><p className="ql-align-right">0503737681&nbsp;او من خلال&nbsp;<a href="https://api.whatsapp.com/send/?phone=966503737681&text&app_absent=0" target="_blank" style={{"color":"var(--color-main-reverse)"}}>الضغ﻿ط هناء</a></p><p><br /></p>
-</div>  )
+    <div className="content-entry p-3">
+      <p><strong>خدمات مجانيه ما بعد البيع.</strong></p>
+      <p><br /></p>
+      <p className="ql-align-justify">هدفنا رضاكم وقمنا بتوفير الخدمات التأليه.</p>
+      <ol>
+        <li className="ql-align-justify">في حالة وصول مقاس المنتج كبير او صغير نقوم بتعديل المقاس.</li>
+        <li className="ql-align-justify">في حالة تغير طلاء المنتج الاكسسوارات يتم صيانة المنتج.</li>
+        <li className="ql-align-justify">في حالة وصول المنتج ويوجد فيه اخطأ مصنعيه يتم تعديله.</li>
+        <li className="ql-align-justify">في حالة تأخر الطلب عن موعده المحدد بإمكانك طلب الإسراع من خلال
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSf9CEmoKl6WTCNCouMA1VRmoG0stFnye0t79PPLRVORHU_Kjw/viewform" target="_blank">
+            <strong> الضغط هنا</strong>
+          </a>
+        </li>
+      </ol>
+      <p className="ql-align-justify"><br /></p>
+      <p className="ql-align-right"><strong>ملاحضات هامة</strong></p>
+      <p className="ql-align-right"><strong>﻿1. في حالة طلب إحداء الخدمات المجانية تكاليف الشحن و التوصيل يدفعها العميل وتتكلف تخصيص بصيانة المنتج مجاناً.</strong></p>
+      <p className="ql-align-right"><strong>﻿2. بامكنك طلب إحداء الخدمات المجانية بالتواصل واتساب</strong></p>
+      <p className="ql-align-right">
+        0503737681 أو من خلال
+        <a href="https://api.whatsapp.com/send/?phone=966503737681&text&app_absent=0" target="_blank" style={{ color: "var(--color-main-reverse)" }}>
+          الضغط هناء
+        </a>
+      </p>
+      <p><br /></p>
+    </div>
+  );
 }
 
-export default page
+export default page;
