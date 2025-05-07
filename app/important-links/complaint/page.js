ea4c584763bf02e923a9aac759ef939f8b5e539d
othman saleh abdulhameed <97992637+thmansaleh@users.noreply.github.com>
2025-05-07 04:05:02 +0400

    Update page.js

--- app/important-links/complaint/page.js
+++ app/important-links/complaint/page.js
@@ -2,10 +2,19 @@ import React from 'react'
 
 function page() {
   return (
-<div className="content-entry p-3">
-<h1 className="font-bold text-2xl mb-6">الشكاوى</h1>
-  <p><strong>في حال كان لديكم مشكلة او خطاء في طلبكم الرجاء زيارة الرابط وتعبئة النموذج وسوف يتم حل المشكلة خلال 24 ساعة عمل</strong></p><p><strong>ونعتذر جداً عن ما حدث معكم</strong></p><p>لتقديم مشكلة <a href="https://docs.google.com/forms/d/e/1FAIpQLSf9CEmoKl6WTCNCouMA1VRmoG0stFnye0t79PPLRVORHU_Kjw/viewform" target="_blank"><strong>اضغط هنا</strong></a></p><p><br /></p>
-</div>  )
+    <div className="content-entry p-3">
+      <h1 className="font-bold text-2xl mb-6">الشكاوى</h1>
+      <p className="mb-2">
+        <strong>في حال وجود مشكلة أو خطأ في طلبكم، نرجو منكم زيارة الرابط أدناه وتعبئة النموذج، وسيتم حل المشكلة خلال 24 ساعة عمل.</strong>
+      </p>
+      <p className="mb-2">
+        <strong>نعتذر لكم بشدة عمّا حدث، ونسعى دائمًا لتحسين تجربتكم معنا.</strong>
+      </p>
+      <p>
+        لتقديم شكوى، <a href="https://docs.google.com/forms/d/e/1FAIpQLSf9CEmoKl6WTCNCouMA1VRmoG0stFnye0t79PPLRVORHU_Kjw/viewform" target="_blank"><strong>اضغط هنا</strong></a>
+      </p>
+    </div>
+  )
 }
 
 export default page
