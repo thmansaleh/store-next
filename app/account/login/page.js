0d733554da60f93583310fb6d711a19420a00a84
othman <thman.saleh@gmail.com>
2025-05-17 01:16:55 +0400

    trfds

--- app/account/login/page.js
+++ app/account/login/page.js
@@ -1,19 +1,9 @@
 'use client'
-import BackArrowBtn from '@/app/components/BackArrowBtn'
-import Header from '@/app/components/header/Header'
-import { Content, EmailVerification } from './Content'
+import { Content } from './Content'
 import { ContactForm } from './ContactForm'
-// import EmailVerification from './EmailVerifaction'
 
 export default function Login(){
-  // return <ContactForm/>
   return <Content/>
-  return <>
-
-  <BackArrowBtn/>
-  {/* <EmailVerification/> */}
-  <EmailVerification/>
-
-</>
+ 
 
 }
